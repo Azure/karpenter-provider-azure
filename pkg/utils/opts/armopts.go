e2e347fe43b32dab4d312b8e29f5db5e02233999
bsoghigian <bsoghigian@microsoft.com>
2023-11-07 16:12:11 -0800

    chore(armclient): removing 429 sdk retries in favor of provider retries

--- pkg/utils/opts/armopts.go
+++ pkg/utils/opts/armopts.go
@@ -18,7 +18,6 @@ package opts
 
 import (
 	"net/http"
-	"time"
 
 	"github.com/Azure/azure-sdk-for-go/sdk/azcore/arm"
 	"github.com/Azure/azure-sdk-for-go/sdk/azcore/policy"
@@ -35,21 +34,7 @@ func DefaultArmOpts() *arm.ClientOptions {
 
 func DefaultRetryOpts() policy.RetryOptions {
 	return policy.RetryOptions{
-		MaxRetries: 20,
-		// Note the default retry behavior is exponential backoff
-		RetryDelay: time.Second * 5,
-		// TODO: bsoghigian: Investigate if we want to leverage some of the status codes other than the defaults.
-		// the defaults are // StatusCodes specifies the HTTP status codes that indicate the operation should be retried.
-		// A nil slice will use the following values.
-		//   http.StatusRequestTimeout      408
-		//   http.StatusTooManyRequests     429
-		//   http.StatusInternalServerError 500
-		//   http.StatusBadGateway          502
-		//   http.StatusServiceUnavailable  503
-		//   http.StatusGatewayTimeout      504
-		// Specifying values will replace the default values.
-		// Specifying an empty slice will disable retries for HTTP status codes.
-		// StatusCodes: nil,
+		MaxRetries: -1,
 	}
 }
 
