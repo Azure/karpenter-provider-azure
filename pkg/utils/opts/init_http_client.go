1000b5b13b5db0ef95ec135d1b547bb77152d9c0
Bryce Soghigian <bsoghigian@gmail.com>
2023-11-22 11:48:16 -0800

    fix: properly closing tcp connections in sdk

--- pkg/utils/opts/init_http_client.go
+++ pkg/utils/opts/init_http_client.go
@@ -17,36 +17,11 @@ limitations under the License.
 package opts
 
 import (
-	"crypto/tls"
-	"net"
-	"net/http"
-	"time"
-
-	"github.com/Azure/go-armbalancer"
+	"github.com/Azure/azure-sdk-for-go-extensions/pkg/middleware"
 )
 
 var defaultHTTPClient *http.Client
 
 func init() {
-	defaultHTTPClient = &http.Client{
-		// For Now using the defaults recommended by Track 2
-		Transport: armbalancer.New(armbalancer.Options{
-			PoolSize: 100,
-			Transport: &http.Transport{
-				Proxy: http.ProxyFromEnvironment,
-				DialContext: (&net.Dialer{
-					Timeout:   30 * time.Second,
-					KeepAlive: 30 * time.Second,
-				}).DialContext,
-				ForceAttemptHTTP2:     true,
-				MaxIdleConns:          100,
-				IdleConnTimeout:       90 * time.Second,
-				TLSHandshakeTimeout:   10 * time.Second,
-				ExpectContinueTimeout: 1 * time.Second,
-				TLSClientConfig: &tls.Config{
-					MinVersion: tls.VersionTLS12,
-				},
-			},
-		}),
-	}
+	defaultHTTPClient =  middleware.DefaultHTTPClient()
 }
