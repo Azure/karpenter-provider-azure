c25c61a6e8200c3e69026d4a46181b70b25d6106
Bryce Soghigian <bsoghigian@gmail.com>
2024-01-10 14:42:47 -0800

    test: parameterizing the test for sku fake

--- pkg/fake/skus_test.go
+++ pkg/fake/skus_test.go
@@ -5,7 +5,7 @@ Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
 
-    http://www.apache.org/licenses/LICENSE-2.0
+	http://www.apache.org/licenses/LICENSE-2.0
 
 Unless required by applicable law or agreed to in writing, software
 distributed under the License is distributed on an "AS IS" BASIS,
@@ -13,7 +13,6 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
-
 package fake
 
 import (
@@ -22,9 +21,8 @@ import (
 	"k8s.io/apimachinery/pkg/util/sets"
 )
 
-// TestSKUExistenceEastUS tests that we are not regressing in our codegen
-func TestSKUExistenceEastUS(t *testing.T) {
-	expectedSKUs := sets.New(
+func TestSKUExistence(t *testing.T) {
+	testSKUExistenceForRegion(t, "eastus", sets.New(
 		"Standard_A0",
 		"Standard_B1s",
 		"Standard_D2s_v3",
@@ -40,26 +38,8 @@ func TestSKUExistenceEastUS(t *testing.T) {
 		"Standard_NC24ads_A100_v4",
 		"Standard_NC6s_v3",
 		"Standard_NC16as_T4_v3",
-	)
-
-	generatedSKUs := ResourceSkus["eastus"]
-
-	skuSets := make(sets.Set[string])
-	for _, sku := range generatedSKUs {
-		skuName := *sku.Name
-		skuSets.Insert(skuName)
-	}
-
-	for _, expectedSKU := range expectedSKUs.UnsortedList() {
-		if exists := skuSets.Has(expectedSKU); !exists {
-			t.Errorf("SKU not found %v", expectedSKU)
-		}
-	}
-}
-
-// TestSKUExistenceWestCentralUS tests that we are not regressing in our codegen
-func TestSKUExistenceWestCentralUS(t *testing.T) {
-	expectedSKUs := sets.New(
+	))
+	testSKUExistenceForRegion(t, "westcentralus", sets.New(
 		"Standard_A0",
 		"Standard_B1s",
 		"Standard_D2s_v3",
@@ -70,9 +50,11 @@ func TestSKUExistenceWestCentralUS(t *testing.T) {
 		"Standard_D64s_v3",
 		"Standard_DS2_v2",
 		"Standard_F16s_v2",
-	)
+	))
+}
 
-	generatedSKUs := ResourceSkus["westcentralus"]
+func testSKUExistenceForRegion(t *testing.T, region string, expectedSKUs sets.Set[string]) {
+	generatedSKUs := ResourceSkus[region]
 
 	skuSets := make(sets.Set[string])
 	for _, sku := range generatedSKUs {
@@ -82,7 +64,7 @@ func TestSKUExistenceWestCentralUS(t *testing.T) {
 
 	for _, expectedSKU := range expectedSKUs.UnsortedList() {
 		if exists := skuSets.Has(expectedSKU); !exists {
-			t.Errorf("SKU not found %v", expectedSKU)
+			t.Errorf("SKU not found in %v: %v", region, expectedSKU)
 		}
 	}
 }
