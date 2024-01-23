25a02b8cbcd8b9b5a0321eba2ac60a091d7dce54
Bryce Soghigian <49734722+Bryce-Soghigian@users.noreply.github.com>
2024-01-23 09:46:54 -0800

    chore: remove provisional image drift implementation (#90)
    * chore: unparam image drift
    
    * test: removing test and un-finished drift code
    
    ---------
    
    Co-authored-by: Alex Leites <18728999+tallaxes@users.noreply.github.com>

--- pkg/cloudprovider/cloudprovider.go
+++ pkg/cloudprovider/cloudprovider.go
@@ -218,11 +218,7 @@ func (c *CloudProvider) IsDrifted(ctx context.Context, nodeClaim *corev1beta1.No
 	if imageVersionDrifted != "" {
 		return imageVersionDrifted, nil
 	}
-	imageDrifted, err := c.isImageDrifted(ctx, nodeClaim, nodePool, nodeClass)
-	if err != nil {
-		return "", err
-	}
-	return imageDrifted, nil
+	return "", nil
 }
 
 // Name returns the CloudProvider implementation name.
--- pkg/cloudprovider/drift.go
+++ pkg/cloudprovider/drift.go
@@ -26,7 +26,6 @@ import (
 	"github.com/Azure/karpenter/pkg/apis/v1alpha2"
 	"github.com/Azure/karpenter/pkg/providers/imagefamily"
 	"github.com/Azure/karpenter/pkg/utils"
-	"github.com/samber/lo"
 
 	v1 "k8s.io/api/core/v1"
 
@@ -123,23 +122,5 @@ func (c *CloudProvider) isImageVersionDrifted(
 		logger.Debugf("drift triggered for %s, with expected image id %s, and actual image id %s", ImageVersionDrift, expectedImageID, vmImageID)
 		return ImageVersionDrift, nil
 	}
-	return "", nil
-}
-
-// TODO: remove nolint on unparam. Added for now in order to pass "make verify"
-// nolint: unparam
-func (c *CloudProvider) isImageDrifted(
-	ctx context.Context, nodeClaim *corev1beta1.NodeClaim, nodePool *corev1beta1.NodePool, _ *v1alpha2.AKSNodeClass) (cloudprovider.DriftReason, error) {
-	instanceTypes, err := c.GetInstanceTypes(ctx, nodePool)
-	if err != nil {
-		return "", fmt.Errorf("getting instanceTypes, %w", err)
-	}
-	_, found := lo.Find(instanceTypes, func(instType *cloudprovider.InstanceType) bool {
-		return instType.Name == nodeClaim.Labels[v1.LabelInstanceTypeStable]
-	})
-	if !found {
-		return "", fmt.Errorf(`finding node instance type "%s"`, nodeClaim.Labels[v1.LabelInstanceTypeStable])
-	}
-
 	return "", nil
 }
