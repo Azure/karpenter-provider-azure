0d93ce4281e91079d994eab13841b784c869a277
Charlie McBride <33269602+charliedmcb@users.noreply.github.com>
2024-02-05 21:57:33 +0000

    Revert "update reference as pkg changed"
    This reverts commit 57919a38c7d3981ac2eddaf1d6486749b94c6fc4.

--- pkg/apis/apis.go
+++ pkg/apis/apis.go
@@ -30,7 +30,7 @@ import (
 	"github.com/Azure/karpenter-provider-azure/pkg/apis/settings"
 	"github.com/Azure/karpenter-provider-azure/pkg/apis/v1alpha2"
 	"sigs.k8s.io/karpenter/pkg/apis"
-	coresettings "sigs.k8s.io/karpenter/pkg/operator/options"
+	coresettings "sigs.k8s.io/karpenter/pkg/apis/settings"
 	"sigs.k8s.io/karpenter/pkg/utils/functional"
 )
 
