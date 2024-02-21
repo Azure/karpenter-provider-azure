406880f08005304046ea033839f4e59ad9a2187b
Bryce Soghigian <bsoghigian@gmail.com>
2024-02-21 15:16:20 -0800

    fix: fixing references to NewProvisioner in tests

--- test/suites/integration/storage_test.go
+++ test/suites/integration/storage_test.go
@@ -64,7 +64,7 @@ var _ = Describe("Dynamic PVC", func() {
 				Name: "azuredisk-claim",
 			},
 			StorageClassName: storageClassName,
-			Resources:        v1.ResourceRequirements{Requests: v1.ResourceList{v1.ResourceStorage: resource.MustParse("5Gi")}},
+			Resources:        v1.VolumeResourceRequirements{Requests: v1.ResourceList{v1.ResourceStorage: resource.MustParse("5Gi")}},
 		})
 
 		pod := test.Pod(test.PodOptions{
@@ -109,7 +109,7 @@ var _ = Describe("Static PVC", func() {
 			},
 			StorageClassName: storageClassName,
 			VolumeName:       pv.Name,
-			Resources:        v1.ResourceRequirements{Requests: v1.ResourceList{v1.ResourceStorage: resource.MustParse("5Gi")}},
+			Resources:        v1.VolumeResourceRequirements{Requests: v1.ResourceList{v1.ResourceStorage: resource.MustParse("5Gi")}},
 		})
 
 		pod := test.Pod(test.PodOptions{
