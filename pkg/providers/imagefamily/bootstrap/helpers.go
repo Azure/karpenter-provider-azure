01fe8f6c9c1def4f0d7c747af005f6c11f621879
Devinwong <wongsiosun@outlook.com>
2024-03-05 02:04:12 +0000

    correct helpers name and add header

--- pkg/providers/imagefamily/bootstrap/helpers.go
+++ pkg/providers/imagefamily/bootstrap/helpers.go
@@ -1,3 +1,19 @@
+/*
+Portions Copyright (c) Microsoft Corporation.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 /* All the helper functions should be hosted by another public repo later. (e.g. agentbaker)
 This action of populating cse_cmd.sh should happen in the Go binary on VHD.
 Therefore, Karpenter will not use these helper functions once the Go binary is ready. */
