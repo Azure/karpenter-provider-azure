30bc8dee7bcba5a1c6b2a1d47a25eb4223881dfb
tallaxes <18728999+tallaxes@users.noreply.github.com>
2024-04-09 07:08:22 +0000

    chore: add crane to toolchain

--- hack/toolchain.sh
+++ hack/toolchain.sh
@@ -23,6 +23,7 @@ tools() {
     go install github.com/onsi/ginkgo/v2/ginkgo@v2.15.0
     go install github.com/rhysd/actionlint/cmd/actionlint@v1.6.26
     go install github.com/mattn/goveralls@v0.0.12
+    go install github.com/google/go-containerregistry/cmd/crane@v0.19.1
 
     if ! echo "$PATH" | grep -q "${GOPATH:-undefined}/bin\|$HOME/go/bin"; then
         echo "Go workspace's \"bin\" directory is not in PATH. Run 'export PATH=\"\$PATH:\${GOPATH:-\$HOME/go}/bin\"'."
@@ -40,7 +41,7 @@ kubebuilder() {
     if [[ "${K8S_VERSION}" = "1.25.x" ]] && [[ "$OSTYPE" == "linux"* ]]; then
         for binary in 'kube-apiserver' 'kubectl'; do
             rm $KUBEBUILDER_ASSETS/$binary
-            wget -P $KUBEBUILDER_ASSETS dl.k8s.io/v1.25.16/bin/linux/${arch}/${binary}
+            wget -P $KUBEBUILDER_ASSETS dl.k8s.io/v1.25.16/bin/linux/"${arch}"/${binary}
             chmod +x $KUBEBUILDER_ASSETS/$binary
         done
     fi
