9a4ffe0730909b1216a49aa44c468242aec0d0d0
Bryce Soghigian <49734722+Bryce-Soghigian@users.noreply.github.com>
2024-04-10 23:19:39 -0700

    chore: cleaning up chart from v0.33 upgrade (#152)
    * chore: removing old crds
    
    * chore: rename old provisioner tags func
    
    * chore: removing references to provisioner in charts
    
    * chore: removing webhooks reference to provisioner crs
    
    * chore: remaining references to provisioner and node template
    
    * fix: ci
    
    ---------
    
    Co-authored-by: Alex Leites <18728999+tallaxes@users.noreply.github.com>

--- pkg/providers/instancetype/suite_test.go
+++ pkg/providers/instancetype/suite_test.go
@@ -393,7 +393,7 @@ var _ = Describe("InstanceType Provider", func() {
 		})
 
 		It("should use ephemeral disk if supported, and set disk size to OSDiskSizeGB from node class", func() {
-			// Create a Provisioner that selects a sku that supports ephemeral
+			// Create a Nodepool that selects a sku that supports ephemeral
 			// SKU Standard_D64s_v3 has 1600GB of CacheDisk space, so we expect we can create an ephemeral disk with size 256GB
 			provider := test.AKSNodeClass()
 			provider.Spec.OSDiskSizeGB = lo.ToPtr[int32](256)
@@ -419,7 +419,7 @@ var _ = Describe("InstanceType Provider", func() {
 			Expect(lo.FromPtr(vm.Properties.StorageProfile.OSDisk.DiffDiskSettings.Option)).To(Equal(armcompute.DiffDiskOptionsLocal))
 		})
 		It("should not use ephemeral disk if ephemeral is supported, but we don't have enough space", func() {
-			// Create a Provisioner that selects a sku that supports ephemeral Standard_D2s_v3
+			// Create a Nodepool that selects a sku that supports ephemeral Standard_D2s_v3
 			// Standard_D2s_V3 has 53GB Of CacheDisk space,
 			// and has 16GB of Temp Disk Space.
 			// With our rule of 100GB being the minimum OSDiskSize, this VM should be created without local disk
@@ -445,7 +445,7 @@ var _ = Describe("InstanceType Provider", func() {
 		})
 	})
 
-	Context("Provisioner with KubeletConfig", func() {
+	Context("Nodepool with KubeletConfig", func() {
 		It("should support provisioning with kubeletConfig, computeResources and maxPods not specified", func() {
 			nodePool.Spec.Template.Spec.Kubelet = &corev1beta1.KubeletConfiguration{
 				PodsPerCore: lo.ToPtr(int32(110)),
@@ -494,7 +494,7 @@ var _ = Describe("InstanceType Provider", func() {
 		})
 	})
 
-	Context("Provisioner with KubeletConfig on a kubenet Cluster", func() {
+	Context("Nodepool with KubeletConfig on a kubenet Cluster", func() {
 		var originalOptions *options.Options
 
 		BeforeEach(func() {
