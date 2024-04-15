d0172b52ae61fa29e2f9a24ae1561770c5448f62
Devinwong <wongsiosun@outlook.com>
2024-04-15 22:29:24 +0000

    add more unit test cases for ensureConfigsNonNil function

--- pkg/providers/imagefamily/bootstrap/aksbootstrap_test.go
+++ pkg/providers/imagefamily/bootstrap/aksbootstrap_test.go
@@ -176,10 +176,40 @@ func Test_ensureConfigsNonNil(t *testing.T) {
 			want: wantedResult,
 		},
 		{
-			name: "Test with non-nil configuration",
+			name: "Test with non-nil configuration and all nil Configs",
+			args: args{
+				v: &nbcontractv1.Configuration{},
+			},
+			want: wantedResult,
+		},
+		{
+			name: "Test with non-nil configuration and partially nil Configs",
 			args: args{
 				v: &nbcontractv1.Configuration{
 					KubeBinaryConfig: &nbcontractv1.KubeBinaryConfig{},
+				},
+			},
+			want: wantedResult,
+		},
+		{
+			name: "Test with non-nil configuration and all non-nil Configs",
+			args: args{
+				v: &nbcontractv1.Configuration{
+					KubeBinaryConfig:         &nbcontractv1.KubeBinaryConfig{},
+					ApiServerConfig:          &nbcontractv1.ApiServerConfig{},
+					AuthConfig:               &nbcontractv1.AuthConfig{},
+					ClusterConfig:            &nbcontractv1.ClusterConfig{},
+					NetworkConfig:            &nbcontractv1.NetworkConfig{},
+					GpuConfig:                &nbcontractv1.GPUConfig{},
+					TlsBootstrappingConfig:   &nbcontractv1.TLSBootstrappingConfig{},
+					KubeletConfig:            &nbcontractv1.KubeletConfig{},
+					RuncConfig:               &nbcontractv1.RuncConfig{},
+					ContainerdConfig:         &nbcontractv1.ContainerdConfig{},
+					TeleportConfig:           &nbcontractv1.TeleportConfig{},
+					CustomLinuxOsConfig:      &nbcontractv1.CustomLinuxOSConfig{},
+					HttpProxyConfig:          &nbcontractv1.HTTPProxyConfig{},
+					CustomCloudConfig:        &nbcontractv1.CustomCloudConfig{},
+					CustomSearchDomainConfig: &nbcontractv1.CustomSearchDomainConfig{},
 				},
 			},
 			want: wantedResult,
