e5f295fe6b8b64f0d990a609ad0118647e16c4e7
Charlie McBride <Charlie.McBride@microsoft.com>
2024-04-19 10:00:13 -0700

    add echo statements to help debug failure

--- hack/release/release.sh
+++ hack/release/release.sh
@@ -1,9 +1,13 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+echo "in release.sh"
+
 SCRIPT_DIR="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
 # shellcheck source=./common.sh
 source "${SCRIPT_DIR}/common.sh"
+
+echo "after source release.sh"
 
 git_tag="$(git describe --exact-match --tags || echo "no tag")"
 if [[ "${git_tag}" == "no tag" ]]; then
@@ -17,5 +21,7 @@ if [[ "$(git status --porcelain)" != "" ]]; then
   echo "There are uncommitted changes, please commit them before releasing."
   exit 1
 fi
+
+echo "before release command call"
 
 release "${commit_sha}" "${git_tag#v}"
