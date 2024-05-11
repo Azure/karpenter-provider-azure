cd895042cc8fd90871f4d0b9353647890f66ab7a
Robin D <comtalyst@outlook.com>
2024-05-11 09:45:21 -0700

    fix: reallowing nodeclaim zones to fix node/nodeclaim creations for workloads with zone affinity constraints (#340)

--- hack/validation/common.sh
+++ hack/validation/common.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-# remove topology.kubernetes.io/zone from allowed labels and requirements
+# remove topology.kubernetes.io/zone from allowed labels and requirements for nodepools
 # until the continuous drift it causes is fixed
-sed -e 's|"topology.kubernetes.io/zone", ||g' -i pkg/apis/crds/karpenter.sh_nodeclaims.yaml
 sed -e 's|"topology.kubernetes.io/zone", ||g' -i pkg/apis/crds/karpenter.sh_nodepools.yaml
