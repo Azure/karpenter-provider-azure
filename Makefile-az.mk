b831dbdac3ccdd373ad319e284f9c9cd622626b3
Bryce Soghigian <49734722+Bryce-Soghigian@users.noreply.github.com>
2024-05-22 13:29:29 -0700

    Update Makefile-az.mk

--- Makefile-az.mk
+++ Makefile-az.mk
@@ -48,7 +48,7 @@ az-mkaks: az-mkacr ## Create test AKS cluster (with --vm-set-type AvailabilitySe
 	az aks get-credentials --name $(AZURE_CLUSTER_NAME) --resource-group $(AZURE_RESOURCE_GROUP) --overwrite-existing
 	skaffold config set default-repo $(AZURE_ACR_NAME).azurecr.io/karpenter
 
-az-mkaks-cniv1: az-mkacr 
+az-mkaks-cniv1: az-mkacr ## Create test AKS cluster (with --network-plugin azure)
 	az aks create          --name $(AZURE_CLUSTER_NAME) --resource-group $(AZURE_RESOURCE_GROUP) --attach-acr $(AZURE_ACR_NAME) \
 		--enable-managed-identity --node-count 3 --generate-ssh-keys -o none --network-plugin azure \
 		--enable-oidc-issuer --enable-workload-identity
