1e43150940b457d50d57f6dcf71d912ce0e9b49e
Bryce Soghigian <bsoghigian@gmail.com>
2024-05-23 09:19:03 -0700

    test: adding an acr suite

---
+++ test/suites/acr/suite_test.go
@@ -0,0 +1,62 @@
+package acr
+
+import (
+	"fmt"
+	"testing"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+	v1 "k8s.io/api/core/v1"
+
+	"k8s.io/apimachinery/pkg/api/resource"
+	"github.com/Azure/karpenter-provider-azure/pkg/apis/v1alpha2"
+	"github.com/Azure/karpenter-provider-azure/test/pkg/environment/azure"
+	corev1beta1 "sigs.k8s.io/karpenter/pkg/apis/v1beta1"
+	"sigs.k8s.io/karpenter/pkg/test"
+)
+
+var env *azure.Environment
+var nodeClass *v1alpha2.AKSNodeClass
+var nodePool *corev1beta1.NodePool
+
+
+func TestAcr(t *testing.T) {
+	RegisterFailHandler(Fail)
+	BeforeSuite(func() {
+		env = azure.NewEnvironment(t)
+	})
+	RunSpecs(t, "Acr")
+}
+
+var _ = BeforeEach(func() { 
+	env.BeforeEach() 
+	nodeClass = env.DefaultAKSNodeClass()
+	nodePool = env.DefaultNodePool(nodeClass)
+})
+var _ = AfterEach(func() { env.Cleanup() })
+var _ = AfterEach(func() { env.AfterEach() })
+
+var _ = Describe("Acr", func() {
+	Describe("Image Pull", func(){
+		It("should allow karpenter user pool nodes to pull images from the clusters attached acr", func(){
+			acrName := env.RetrieveACRName()
+			deployment := test.Deployment(test.DeploymentOptions{
+				Replicas: 10,
+				PodOptions: test.PodOptions{
+					ResourceRequirements: v1.ResourceRequirements{
+						Requests: v1.ResourceList{
+							v1.ResourceCPU: resource.MustParse("1.1"),
+						},
+					},
+					Image: fmt.Sprintf("%s/pause:3.6",acrName),
+				},
+			})
+
+
+			env.ExpectCreated(nodePool, nodeClass, deployment)
+
+
+
+		})
+	})
+})
