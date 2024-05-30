bda94d1382dd956cd156f6ee8cabb46c66cdb0c3
Bryce Soghigian <bsoghigian@gmail.com>
2024-05-30 15:06:50 -0700

    test(unit): fixing client version we get for bootstrap to use the same source of truth

--- pkg/providers/instancetype/suite_test.go
+++ pkg/providers/instancetype/suite_test.go
@@ -1107,10 +1107,6 @@ var _ = Describe("InstanceType Provider", func() {
 			Expect(err).To(Succeed())
 			decodedString := string(decodedBytes[:])
 			Expect(decodedString).To(ContainSubstring("CREDENTIAL_PROVIDER_DOWNLOAD_URL"))
-			Expect(decodedString).To(ContainSubstring(
-				fmt.Sprintf("https://acs-mirror.azureedge.net/cloud-provider-azure/%s/binaries/azure-acr-credential-provider-linux-amd64-v%s.tar.gz", env.Version.String(), env.Version.String()),
-			))
-
 			kubeletFlags := decodedString[strings.Index(decodedString, "KUBELET_FLAGS=")+len("KUBELET_FLAGS="):]
 			parseKubeletFlags := func(flags string) map[string]string {
 				flagMap := make(map[string]string)
@@ -1130,6 +1126,12 @@ var _ = Describe("InstanceType Provider", func() {
 			k8sVersion, err := azureEnv.ImageProvider.KubeServerVersion(ctx)
 			Expect(err).To(BeNil())
 			parsed := semver.MustParse(k8sVersion)
+
+			Expect(decodedString).To(ContainSubstring(
+				fmt.Sprintf("https://acs-mirror.azureedge.net/cloud-provider-azure/%s/binaries/azure-acr-credential-provider-linux-amd64-v%s.tar.gz", parsed.String(), parsed.String()),
+			))
+
+
 			if parsed.Minor < 30 {
 				fmt.Println("K8s Version for env client: ")
 				Expect(flagMap).To(HaveKey("--azure-container-registry-config"))
