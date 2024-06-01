b92539f7c266741ab927b1e6cbd4e82a63303ddb
Bryce Soghigian <bsoghigian@gmail.com>
2024-06-01 10:21:03 -0700

    fix: use different driver version for 550

--- pkg/utils/gpu.go
+++ pkg/utils/gpu.go
@@ -23,7 +23,7 @@ import (
 // TODO: Get these from agentbaker
 const (
 	Nvidia470CudaDriverVersion = "cuda-470.82.01"
-	Nvidia550CudaDriverVersion = "cuda-535.54.03"
+	Nvidia550CudaDriverVersion = "cuda-535.54.15"
 	Nvidia535GridDriverVersion = "grid-535.161.08"
 
 	AKSGPUGridSHA = "sha-d1f0ca"
