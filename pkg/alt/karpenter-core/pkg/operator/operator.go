3b5d8d909e978ee0173702981d3bf2730e2ceb18
tallaxes <18728999+tallaxes@users.noreply.github.com>
2024-10-10 07:48:43 +0000

    chore: update the alt operator

--- pkg/alt/karpenter-core/pkg/operator/operator.go
+++ pkg/alt/karpenter-core/pkg/operator/operator.go
@@ -30,34 +30,39 @@ import (
 	"github.com/go-logr/zapr"
 	"github.com/samber/lo"
 
-	"sigs.k8s.io/karpenter/pkg/apis/v1beta1"
 	"sigs.k8s.io/karpenter/pkg/events"
 
 	coreoperator "sigs.k8s.io/karpenter/pkg/operator"
-	coreoperatorlogging "sigs.k8s.io/karpenter/pkg/operator/logging"
 
 	coordinationv1 "k8s.io/api/coordination/v1"
-	v1 "k8s.io/api/core/v1"
+	corev1 "k8s.io/api/core/v1"
+	storagev1 "k8s.io/api/storage/v1"
+
 	"k8s.io/apimachinery/pkg/fields"
 	"sigs.k8s.io/karpenter/pkg/operator/injection"
 	"sigs.k8s.io/karpenter/pkg/operator/options"
-	"sigs.k8s.io/karpenter/pkg/operator/scheme"
 
-	//"k8s.io/apimachinery/pkg/fields"
 	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/kubernetes/scheme"
 	"k8s.io/client-go/rest"
 	"k8s.io/client-go/tools/leaderelection/resourcelock"
 	"k8s.io/client-go/util/flowcontrol"
 	"k8s.io/utils/clock"
 	knativeinjection "knative.dev/pkg/injection"
-	knativelogging "knative.dev/pkg/logging"
 	"knative.dev/pkg/signals"
 	"knative.dev/pkg/system"
-	controllerruntime "sigs.k8s.io/controller-runtime"
+	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/cache"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/healthz"
 	"sigs.k8s.io/controller-runtime/pkg/metrics/server"
+
+	"sigs.k8s.io/karpenter/pkg/operator/logging"
+
+	"k8s.io/klog/v2"
+	"sigs.k8s.io/controller-runtime/pkg/log"
+
+	v1 "sigs.k8s.io/karpenter/pkg/apis/v1"
 )
 
 // Unmodified; for exposing private entity only
@@ -101,12 +106,17 @@ func NewOperator() (context.Context, *coreoperator.Operator) {
 		debug.SetMemoryLimit(newLimit)
 	}
 
+	// Logging
+	logger := zapr.NewLogger(logging.NewLogger(ccPlaneCtx, component))
+	log.SetLogger(logger)
+	klog.SetLogger(logger)
+
 	// Webhook
 	// Unsupported -- skipping
 
 	// Client Config
 	ccPlaneConfig := lo.Must(rest.InClusterConfig())
-	overlayConfig := controllerruntime.GetConfigOrDie()
+	overlayConfig := ctrl.GetConfigOrDie()
 	ccPlaneConfig.RateLimiter = flowcontrol.NewTokenBucketRateLimiter(float32(options.FromContext(ccPlaneCtx).KubeClientQPS), options.FromContext(ccPlaneCtx).KubeClientBurst)
 	overlayConfig.RateLimiter = flowcontrol.NewTokenBucketRateLimiter(float32(options.FromContext(overlayCtx).KubeClientQPS), options.FromContext(overlayCtx).KubeClientBurst)
 	// config.UserAgent = fmt.Sprintf("%s/%s", appName, Version)
@@ -115,30 +125,22 @@ func NewOperator() (context.Context, *coreoperator.Operator) {
 
 	// Client
 	overlayKubernetesInterface := kubernetes.NewForConfigOrDie(overlayConfig)
-	//configMapWatcher := informer.NewInformedWatcher(ccPlaneKubernetesInterface, system.Namespace())
-	//lo.Must0(configMapWatcher.Start(ccPlaneCtx.Done()))
-
-	// Logging
-	logger := coreoperatorlogging.NewLogger(ccPlaneCtx, component)
-	ccPlaneCtx = knativelogging.WithLogger(ccPlaneCtx, logger)
-	overlayCtx = knativelogging.WithLogger(overlayCtx, logger)
-	coreoperatorlogging.ConfigureGlobalLoggers(ccPlaneCtx)
 
 	// Manager
-	mgrOpts := controllerruntime.Options{
-		Logger:                     ignoreDebugEvents(zapr.NewLogger(logger.Desugar())),
-		LeaderElection:             options.FromContext(overlayCtx).EnableLeaderElection,
-		LeaderElectionID:           "karpenter-leader-election",
-		LeaderElectionResourceLock: resourcelock.LeasesResourceLock,
-		Scheme:                     scheme.Scheme,
+	mgrOpts := ctrl.Options{
+		Logger:                        logging.IgnoreDebugEvents(logger),
+		LeaderElection:                !options.FromContext(overlayCtx).DisableLeaderElection,
+		LeaderElectionID:              "karpenter-leader-election",
+		LeaderElectionResourceLock:    resourcelock.LeasesResourceLock,
+		LeaderElectionNamespace:       system.Namespace(),
+		LeaderElectionReleaseOnCancel: true,
+		Scheme:                        scheme.Scheme,
 		Metrics: server.Options{
 			BindAddress: fmt.Sprintf(":%d", options.FromContext(overlayCtx).MetricsPort),
 		},
 		HealthProbeBindAddress: fmt.Sprintf(":%d", options.FromContext(overlayCtx).HealthProbePort),
 		BaseContext: func() context.Context {
-			ctx := context.Background()
-			ctx = knativelogging.WithLogger(ctx, logger)
-			// ctx = injection.WithConfig(ctx, overlayConfig)
+			ctx := log.IntoContext(context.Background(), logger)
 			ctx = injection.WithOptionsOrDie(ctx, options.Injectables...)
 			return ctx
 		},
@@ -164,31 +166,50 @@ func NewOperator() (context.Context, *coreoperator.Operator) {
 			"/debug/pprof/threadcreate": pprof.Handler("threadcreate"),
 		})
 	}
-	mgr, err := controllerruntime.NewManager(overlayConfig, mgrOpts)
+	mgr, err := ctrl.NewManager(overlayConfig, mgrOpts)
 	mgr = lo.Must(mgr, err, "failed to setup manager")
 
-	lo.Must0(mgr.GetFieldIndexer().IndexField(overlayCtx, &v1.Pod{}, "spec.nodeName", func(o client.Object) []string {
-		return []string{o.(*v1.Pod).Spec.NodeName}
+	lo.Must0(mgr.GetFieldIndexer().IndexField(overlayCtx, &corev1.Pod{}, "spec.nodeName", func(o client.Object) []string {
+		return []string{o.(*corev1.Pod).Spec.NodeName}
 	}), "failed to setup pod indexer")
-	lo.Must0(mgr.GetFieldIndexer().IndexField(overlayCtx, &v1.Node{}, "spec.providerID", func(o client.Object) []string {
-		return []string{o.(*v1.Node).Spec.ProviderID}
+	lo.Must0(mgr.GetFieldIndexer().IndexField(overlayCtx, &corev1.Node{}, "spec.providerID", func(o client.Object) []string {
+		return []string{o.(*corev1.Node).Spec.ProviderID}
 	}), "failed to setup node provider id indexer")
 	lo.Must0(func() error {
 		_, _, err := lo.AttemptWithDelay(42, 10*time.Second, func(index int, duration time.Duration) error {
-			err := mgr.GetFieldIndexer().IndexField(overlayCtx, &v1beta1.NodeClaim{}, "status.providerID", func(o client.Object) []string {
-				return []string{o.(*v1beta1.NodeClaim).Status.ProviderID}
+			err := mgr.GetFieldIndexer().IndexField(overlayCtx, &v1.NodeClaim{}, "status.providerID", func(o client.Object) []string {
+				return []string{o.(*v1.NodeClaim).Status.ProviderID}
 			})
 			if err != nil {
-				knativelogging.FromContext(ccPlaneCtx).Infof("failed to setup machine provider id indexer, CRDs deployment may not be ready, index: %d, duration: %v, err: %v", index, duration, err)
+				log.FromContext(ccPlaneCtx).WithValues("index", index, "duration", duration, "err", err).Info("failed to setup NodeClaim provider id indexer, CRDs deployment may not be ready")
 			}
 			return err
 		})
 		return err
 	}(), "failed to setup nodeclaim provider id indexer, all attempts used")
 
-	lo.Must0(mgr.AddReadyzCheck("manager", func(req *http.Request) error {
-		return lo.Ternary(mgr.GetCache().WaitForCacheSync(req.Context()), nil, fmt.Errorf("failed to sync caches"))
-	}))
+	lo.Must0(mgr.GetFieldIndexer().IndexField(overlayCtx, &v1.NodeClaim{}, "spec.nodeClassRef.group", func(o client.Object) []string {
+		return []string{o.(*v1.NodeClaim).Spec.NodeClassRef.Group}
+	}), "failed to setup nodeclaim nodeclassref apiversion indexer")
+	lo.Must0(mgr.GetFieldIndexer().IndexField(overlayCtx, &v1.NodeClaim{}, "spec.nodeClassRef.kind", func(o client.Object) []string {
+		return []string{o.(*v1.NodeClaim).Spec.NodeClassRef.Kind}
+	}), "failed to setup nodeclaim nodeclassref kind indexer")
+	lo.Must0(mgr.GetFieldIndexer().IndexField(overlayCtx, &v1.NodeClaim{}, "spec.nodeClassRef.name", func(o client.Object) []string {
+		return []string{o.(*v1.NodeClaim).Spec.NodeClassRef.Name}
+	}), "failed to setup nodeclaim nodeclassref name indexer")
+	lo.Must0(mgr.GetFieldIndexer().IndexField(overlayCtx, &v1.NodePool{}, "spec.template.spec.nodeClassRef.group", func(o client.Object) []string {
+		return []string{o.(*v1.NodePool).Spec.Template.Spec.NodeClassRef.Group}
+	}), "failed to setup nodepool nodeclassref apiversion indexer")
+	lo.Must0(mgr.GetFieldIndexer().IndexField(overlayCtx, &v1.NodePool{}, "spec.template.spec.nodeClassRef.kind", func(o client.Object) []string {
+		return []string{o.(*v1.NodePool).Spec.Template.Spec.NodeClassRef.Kind}
+	}), "failed to setup nodepool nodeclassref kind indexer")
+	lo.Must0(mgr.GetFieldIndexer().IndexField(overlayCtx, &v1.NodePool{}, "spec.template.spec.nodeClassRef.name", func(o client.Object) []string {
+		return []string{o.(*v1.NodePool).Spec.Template.Spec.NodeClassRef.Name}
+	}), "failed to setup nodepool nodeclassref name indexer")
+	lo.Must0(mgr.GetFieldIndexer().IndexField(overlayCtx, &storagev1.VolumeAttachment{}, "spec.nodeName", func(o client.Object) []string {
+		return []string{o.(*storagev1.VolumeAttachment).Spec.NodeName}
+	}), "failed to setup volumeattachment indexer")
+
 	lo.Must0(mgr.AddHealthzCheck("healthz", healthz.Ping))
 	lo.Must0(mgr.AddReadyzCheck("readyz", healthz.Ping))
 
