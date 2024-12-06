2aa5c11889c461944094c75840b8cdd3d3b1cc14
Bryce Soghigian <bsoghigian@gmail.com>
2024-12-06 15:21:20 -0800

    fix: goldpinger separation

--- test/suites/networking/suite_test.go
+++ test/suites/networking/suite_test.go
@@ -16,6 +16,7 @@ import (
 	corev1 "k8s.io/api/core/v1"
 	rbacv1 "k8s.io/api/rbac/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/labels"
 	"k8s.io/apimachinery/pkg/util/intstr"
 	"k8s.io/utils/ptr"
 
@@ -23,24 +24,23 @@ import (
 
 	"github.com/Azure/karpenter-provider-azure/pkg/apis/v1alpha2"
 	"github.com/Azure/karpenter-provider-azure/test/pkg/environment/azure"
-	corev1beta1 "sigs.k8s.io/karpenter/pkg/apis/v1beta1"
-	//"sigs.k8s.io/karpenter/pkg/test"
+	karpv1 "sigs.k8s.io/karpenter/pkg/apis/v1"
 )
-
-
 
 var env *azure.Environment
 var nodeClass *v1alpha2.AKSNodeClass
-var nodePool *corev1beta1.NodePool
+var nodePool *karpv1.NodePool
 var ns string
+
 func TestNetworking(t *testing.T) {
 	RegisterFailHandler(Fail)
-	BeforeSuite(func(){
+	BeforeSuite(func() {
 		env = azure.NewEnvironment(t)
 		ns = "default"
 	})
 	AfterSuite(func() {
 		By("Cleaning up Goldpinger resources")
+		// TODO: Move into env.Cleanup()
 		env.ExpectDeleted(
 			&corev1.ServiceAccount{ObjectMeta: metav1.ObjectMeta{Name: "goldpinger-serviceaccount", Namespace: ns}},
 			&rbacv1.ClusterRole{ObjectMeta: metav1.ObjectMeta{Name: "goldpinger-clusterrole"}},
@@ -58,70 +58,57 @@ var _ = BeforeEach(func() { env.BeforeEach() })
 var _ = AfterEach(func() { env.Cleanup() })
 var _ = AfterEach(func() { env.AfterEach() })
 
-
 var _ = Describe("Networking", func() {
-	Describe("GoldPinger", func(){
-	It("should ensure goldpinger resources are all deployed and are reachable", func() {
-		nodeClass := env.DefaultAKSNodeClass()
-		nodePool := env.DefaultNodePool(nodeClass)
-		env.ExpectCreated(nodeClass, nodePool)
+	Describe("GoldPinger", func() {
+		It("should ensure goldpinger resources are all deployed", func() {
+			nodeClass := env.DefaultAKSNodeClass()
+			nodePool := env.DefaultNodePool(nodeClass)
+			env.ExpectCreated(nodeClass, nodePool)
 
-		By("should configure all k8s resources needed")
-		serviceAccount := createServiceAccount(ns)
-		clusterRole := createClusterRole()
-		clusterRoleBinding := createClusterRoleBinding(ns)
-		daemonSet := createDaemonSet(ns)
-		service := createService(ns)
-		deployment := createDeployment(ns)
-	
-		env.ExpectCreated(serviceAccount, clusterRole, clusterRoleBinding, daemonSet, service, deployment)
-		By("should scale up the goldpinger-deploy pods for pod to pod connectivity testing and to scale up karp nodes")
-		Eventually(func() int {
-			pods := &corev1.PodList{}
-			err := env.Client.List(context.TODO(), pods, client.MatchingLabels{"app": "goldpinger"})
-			Expect(err).NotTo(HaveOccurred(), "Failed to list Goldpinger pods")
-			readyCount := 0
-			for _, pod := range pods.Items {
-				for _, condition := range pod.Status.Conditions {
-					if condition.Type == corev1.PodReady && condition.Status == corev1.ConditionTrue {
-						readyCount++
-					}
+			By("should configure all k8s resources needed")
+			serviceAccount := createServiceAccount(ns)
+			clusterRole := createClusterRole()
+			clusterRoleBinding := createClusterRoleBinding(ns)
+			daemonSet := createDaemonSet(ns)
+			service := createService(ns)
+			deployment := createDeployment(ns)
+
+			env.ExpectCreated(serviceAccount, clusterRole, clusterRoleBinding, daemonSet, service, deployment)
+			By("should scale up the goldpinger-deploy pods for pod to pod connectivity testing and to scale up karp nodes")
+
+			env.ExpectCreatedNodeCount("==", 10)
+			env.EventuallyExpectHealthyPodCountWithTimeout(time.Minute*15, labels.SelectorFromSet(deployment.Spec.Selector.MatchLabels), 10)
+			By("should ensure gold pinger service has clusterIP assigned")
+			Eventually(func() string {
+				svc := &corev1.Service{}
+				err := env.Client.Get(context.TODO(), client.ObjectKey{Name: "goldpinger", Namespace: ns}, svc)
+				if err != nil {
+					return ""
 				}
-			}
-			return readyCount
-		}, 15*time.Minute, 10*time.Second).Should(BeNumerically(">=", 10), "Not all Goldpinger pods are ready")
-		By("should ensure gold pinger service has clusterIP assigned")
-		Eventually(func() string {
-			svc := &corev1.Service{}
-			err := env.Client.Get(context.TODO(), client.ObjectKey{Name: "goldpinger", Namespace: ns}, svc)
-			if err != nil {
-				return ""
-			}
-			return svc.Spec.ClusterIP
-	}, 2*time.Minute, 10*time.Second).ShouldNot(BeEmpty(), "Goldpinger service ClusterIP not assigned")
-	By("Fetching node connectivity status from Goldpinger")
-		resp, err := http.Get("http://goldpinger.default.svc.cluster.local:8080/check_all")
-		Expect(err).NotTo(HaveOccurred(), "Failed to reach Goldpinger service")
-		defer resp.Body.Close()
-					
-		body, err := ioutil.ReadAll(resp.Body)
-		Expect(err).NotTo(HaveOccurred(), "Failed to read Goldpinger response body")
-
-		var checkAllResponse CheckAllResponse
-		err = json.Unmarshal(body, &checkAllResponse)
-		Expect(err).NotTo(HaveOccurred(), "Failed to parse Goldpinger response JSON")
+				return svc.Spec.ClusterIP
+			}, 2*time.Minute, 10*time.Second).ShouldNot(BeEmpty(), "Goldpinger service ClusterIP not assigned")
+			By("Fetching node connectivity status from Goldpinger")
+			resp, err := http.Get("http://goldpinger.default.svc.cluster.local:8080/check_all")
+			Expect(err).NotTo(HaveOccurred(), "Failed to reach Goldpinger service")
+			defer resp.Body.Close()
 		
-		for node, status := range checkAllResponse.Nodes {
-			// This checks that all other nodes in the cluster can reach this node
-			Expect(status.Status).To(Equal("ok"), fmt.Sprintf("Node %s is not reachable", node))
-		}
-		// TODO: Check pod stats to see if pod to pod communciation works 
-		time.Sleep(time.Hour * 1)
-	})		
-	})
-	
-})
+			body, err := ioutil.ReadAll(resp.Body)
+			Expect(err).NotTo(HaveOccurred(), "Failed to read Goldpinger response body")
 
+			var checkAllResponse CheckAllResponse
+			err = json.Unmarshal(body, &checkAllResponse)
+			Expect(err).NotTo(HaveOccurred(), "Failed to parse Goldpinger response JSON")
+
+			for node, status := range checkAllResponse.Nodes {
+				// This checks that all other nodes in the cluster can reach this node
+				Expect(status.Status).To(Equal("ok"), fmt.Sprintf("Node %s is not reachable", node))
+			}
+			// TODO: Check pod stats to see if pod to pod communciation works
+			time.Sleep(time.Hour * 1)
+		})
+	})
+
+})
 
 // --------------------- Test Helpers ------------------------ //
 type NodeStatus struct {
@@ -130,9 +117,9 @@ type NodeStatus struct {
 }
 
 type CheckAllResponse struct {
-    Nodes map[string]NodeStatus            `json:"nodes"`        // For node-to-node connectivity
-    Pods  map[string]map[string]NodeStatus `json:"pods"`         // For pod-to-pod reachability
-    PacketLoss map[string]float64          `json:"packet_loss"`  // For packet loss (if it occurred) 
+	Nodes      map[string]NodeStatus            `json:"nodes"`       // For node-to-node connectivity
+	Pods       map[string]map[string]NodeStatus `json:"pods"`        // For pod-to-pod reachability
+	PacketLoss map[string]float64               `json:"packet_loss"` // For packet loss (if it occurred)
 }
 
 func createServiceAccount(namespace string) *corev1.ServiceAccount {
@@ -224,26 +211,25 @@ func createDaemonSet(namespace string) *appsv1.DaemonSet {
 	}
 }
 
-
 func createService(namespace string) *corev1.Service {
-    return &corev1.Service{
-        ObjectMeta: metav1.ObjectMeta{
-            Name:      "goldpinger",
-            Namespace: namespace,
-            Labels:    map[string]string{"app": "goldpinger"},
-        },
-        Spec: corev1.ServiceSpec{
-            Type: corev1.ServiceTypeNodePort,
-            Ports: []corev1.ServicePort{
-                {
-                    Port:       8080,
-                    TargetPort: intstr.FromInt(8080),
-                    Name:       "http",
-                },
-            },
-            Selector: map[string]string{"app": "goldpinger"},
-        },
-    }
+	return &corev1.Service{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "goldpinger",
+			Namespace: namespace,
+			Labels:    map[string]string{"app": "goldpinger"},
+		},
+		Spec: corev1.ServiceSpec{
+			Type: corev1.ServiceTypeNodePort,
+			Ports: []corev1.ServicePort{
+				{
+					Port:       8080,
+					TargetPort: intstr.FromInt(8080),
+					Name:       "http",
+				},
+			},
+			Selector: map[string]string{"app": "goldpinger"},
+		},
+	}
 }
 
 func createDeployment(namespace string) *appsv1.Deployment {
@@ -266,14 +252,15 @@ func createDeployment(namespace string) *appsv1.Deployment {
 					// We want to validate node to node communication so we need to spread the deployment between many karpenter nodes
 					TopologySpreadConstraints: []corev1.TopologySpreadConstraint{
 						{
-							MaxSkew: 1,
-							TopologyKey: "kubernetes.io/hostname",
+							MaxSkew:           1,
+							TopologyKey:       "kubernetes.io/hostname",
 							WhenUnsatisfiable: corev1.DoNotSchedule,
 							LabelSelector: &metav1.LabelSelector{
 								MatchLabels: map[string]string{"app": "goldpinger"},
 							},
 						},
 					},
+					// TODO: Contribute ServiceAccountName and Containers to the karpenter-core test.PodOptions
 					ServiceAccountName: "goldpinger-serviceaccount",
 					Containers: []corev1.Container{
 						{
@@ -289,4 +276,3 @@ func createDeployment(namespace string) *appsv1.Deployment {
 		},
 	}
 }
-
