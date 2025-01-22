7b78fd59c29e48455384ff848ca4e846833f6c54
Bryce Soghigian <bsoghigian@gmail.com>
2025-01-21 20:47:18 -0800

    fix: test

--- pkg/test/expectations/expectations.go
+++ pkg/test/expectations/expectations.go
@@ -24,9 +24,6 @@ import (
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 
-	. "github.com/onsi/ginkgo/v2"
-	. "github.com/onsi/gomega"
-
 	"github.com/Azure/karpenter-provider-azure/pkg/fake"
 	"github.com/Azure/karpenter-provider-azure/pkg/test"
 )
