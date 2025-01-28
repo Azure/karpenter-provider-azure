fe0c4a06a967adf3df8e596ab07aafb4858e6a1c
Bryce Soghigian <49734722+Bryce-Soghigian@users.noreply.github.com>
2025-01-28 02:19:28 +0000

    revert: removing lo.FromPtr checks for nodeclaim creation to avoid creating nodeclaims with empty required properties

--- pkg/cloudprovider/cloudprovider.go
+++ pkg/cloudprovider/cloudprovider.go
@@ -339,7 +339,7 @@ func (c *CloudProvider) instanceToNodeClaim(ctx context.Context, vm *armcompute.
 	labels[karpv1.CapacityTypeLabelKey] = instance.GetCapacityType(vm)
 
 	if tag, ok := vm.Tags[instance.NodePoolTagKey]; ok {
-		labels[karpv1.NodePoolLabelKey] = lo.FromPtr(tag)
+		labels[karpv1.NodePoolLabelKey] = *tag
 	}
 
 	inPlaceUpdateHash, err := inplaceupdate.HashFromVM(vm)
@@ -348,15 +348,15 @@ func (c *CloudProvider) instanceToNodeClaim(ctx context.Context, vm *armcompute.
 	}
 	annotations[v1alpha2.AnnotationInPlaceUpdateHash] = inPlaceUpdateHash
 
-	nodeClaim.Name = GenerateNodeClaimName(lo.FromPtr(vm.Name))
+	nodeClaim.Name = GenerateNodeClaimName(*vm.Name)
 	nodeClaim.Labels = labels
 	nodeClaim.Annotations = annotations
-	nodeClaim.CreationTimestamp = metav1.Time{Time: lo.FromPtr(vm.Properties.TimeCreated)}
+	nodeClaim.CreationTimestamp = metav1.Time{Time: *vm.Properties.TimeCreated}
 	// Set the deletionTimestamp to be the current time if the instance is currently terminating
 	if utils.IsVMDeleting(*vm) {
 		nodeClaim.DeletionTimestamp = &metav1.Time{Time: time.Now()}
 	}
-	nodeClaim.Status.ProviderID = utils.ResourceIDToProviderID(ctx, lo.FromPtr(vm.ID))
+	nodeClaim.Status.ProviderID = utils.ResourceIDToProviderID(ctx, *vm.ID)
 	if vm.Properties != nil && vm.Properties.StorageProfile != nil && vm.Properties.StorageProfile.ImageReference != nil {
 		nodeClaim.Status.ImageID = utils.ImageReferenceToString(vm.Properties.StorageProfile.ImageReference)
 	}
