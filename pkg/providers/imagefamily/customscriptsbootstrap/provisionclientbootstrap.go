9b02fa6eb5db4a6f63f93da88de52c46f14a974f
tomabraham <tomabraham@microsoft.com>
2025-01-30 16:09:38 -0800

    adding logging round tripper

--- pkg/providers/imagefamily/customscriptsbootstrap/provisionclientbootstrap.go
+++ pkg/providers/imagefamily/customscriptsbootstrap/provisionclientbootstrap.go
@@ -20,10 +20,13 @@ import (
 	"context"
 	"encoding/base64"
 	"fmt"
+	"log/slog"
 	"math"
+	"os"
 	"strings"
 	"time"
 
+	"github.com/Azure/aks-middleware/http/client/direct/restlogger"
 	"github.com/Azure/azure-sdk-for-go/sdk/azcore/to"
 	"github.com/Azure/karpenter-provider-azure/pkg/apis/v1alpha2"
 	"github.com/Azure/karpenter-provider-azure/pkg/operator/options"
@@ -166,6 +169,11 @@ func (p ProvisionClientBootstrap) GetCustomDataAndCSE(ctx context.Context) (stri
 
 func (p *ProvisionClientBootstrap) getNodeBootstrappingFromClient(ctx context.Context, provisionProfile *models.ProvisionProfile, provisionHelperValues *models.ProvisionHelperValues, bootstrapToken string) (string, string, error) {
 	transport := httptransport.New(options.FromContext(ctx).NodeBootstrappingServerURL, "/", []string{"http"})
+	
+	logger := slog.New(slog.NewJSONHandler(os.Stdout, nil))
+	loggingClient := restlogger.NewLoggingClient(logger)
+	transport.Transport = loggingClient.Transport
+
 	client := client.New(transport, strfmt.Default)
 
 	params := operations.NewNodeBootstrappingGetParams()
