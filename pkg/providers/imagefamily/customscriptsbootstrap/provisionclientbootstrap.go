6fa752448499d2efd42c7240534bd50b8d078c96
Bryce Soghigian <49734722+Bryce-Soghigian@users.noreply.github.com>
2025-02-12 23:09:17 +0000

    fix: make presubmit

--- pkg/providers/imagefamily/customscriptsbootstrap/provisionclientbootstrap.go
+++ pkg/providers/imagefamily/customscriptsbootstrap/provisionclientbootstrap.go
@@ -169,7 +169,7 @@ func (p ProvisionClientBootstrap) GetCustomDataAndCSE(ctx context.Context) (stri
 
 func (p *ProvisionClientBootstrap) getNodeBootstrappingFromClient(ctx context.Context, provisionProfile *models.ProvisionProfile, provisionHelperValues *models.ProvisionHelperValues, bootstrapToken string) (string, string, error) {
 	transport := httptransport.New(options.FromContext(ctx).NodeBootstrappingServerURL, "/", []string{"http"})
-	
+
 	logger := slog.New(slog.NewJSONHandler(os.Stdout, nil))
 	loggingClient := restlogger.NewLoggingClient(logger)
 	transport.Transport = loggingClient.Transport
@@ -191,7 +191,7 @@ func (p *ProvisionClientBootstrap) getNodeBootstrappingFromClient(ctx context.Co
 
 	resp, err := client.Operations.NodeBootstrappingGet(params)
 	if err != nil {
-		// As of now we just fail the provisioning given the unlikely scenario of retriable error, but could be revisted along with retriable status on the server side.
+		// As of now we just fail the provisioning given the unlikely scenario of retriable error, but could be revisited along with retriable status on the server side.
 		return "", "", err
 	}
 
@@ -247,7 +247,7 @@ func normalizeResourceGroupNameForLabel(resourceGroupName string) string {
 }
 
 func reverseVMMemoryOverhead(vmMemoryOverheadPercent float64, adjustedMemory float64) float64 {
-	// This is not the best way to do it... But will be refactored later, given that retreiving the original memory properly might involves some restructure.
+	// This is not the best way to do it... But will be refactored later, given that retrieving the original memory properly might involves some restructure.
 	// Due to the fact that it is abstracted behind the cloudprovider interface.
 	return adjustedMemory / (1 - vmMemoryOverheadPercent)
 }
