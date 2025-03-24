8e70b1b2353bf8e139b99e9a036ca51b4ea6f5d5
Charlie McBride <33269602+charliedmcb@users.noreply.github.com>
2025-03-24 20:38:16 +0000

    switched to HaveExactElements for tests, and removed alphabetical sorting, since it is already priority sorted

--- pkg/controllers/nodeclass/status/nodeimage_test.go
+++ pkg/controllers/nodeclass/status/nodeimage_test.go
@@ -99,7 +99,7 @@ var _ = Describe("NodeClass NodeImage Status Controller", func() {
 		nodeClass = ExpectExists(ctx, env.Client, nodeClass)
 
 		Expect(len(nodeClass.Status.NodeImages)).To(Equal(3))
-		Expect(nodeClass.Status.NodeImages).To(ContainElements(getExpectedTestCommunityImages(newCIGImageVersion)))
+		Expect(nodeClass.Status.NodeImages).To(HaveExactElements(getExpectedTestCommunityImages(newCIGImageVersion)))
 		Expect(nodeClass.StatusConditions().IsTrue(v1alpha2.ConditionTypeNodeImagesReady)).To(BeTrue())
 	})
 
@@ -111,14 +111,14 @@ var _ = Describe("NodeClass NodeImage Status Controller", func() {
 		ExpectApplied(ctx, env.Client, nodeClass)
 		nodeClass = ExpectExists(ctx, env.Client, nodeClass)
 
-		Expect(nodeClass.Status.NodeImages).To(ContainElements(getExpectedTestCommunityImages(oldcigImageVersion)))
+		Expect(nodeClass.Status.NodeImages).To(HaveExactElements(getExpectedTestCommunityImages(oldcigImageVersion)))
 		Expect(nodeClass.StatusConditions().IsTrue(v1alpha2.ConditionTypeNodeImagesReady)).To(BeTrue())
 
 		ExpectObjectReconciled(ctx, env.Client, controller, nodeClass)
 		nodeClass = ExpectExists(ctx, env.Client, nodeClass)
 
 		Expect(len(nodeClass.Status.NodeImages)).To(Equal(3))
-		Expect(nodeClass.Status.NodeImages).To(ContainElements(getExpectedTestCommunityImages(newCIGImageVersion)))
+		Expect(nodeClass.Status.NodeImages).To(HaveExactElements(getExpectedTestCommunityImages(newCIGImageVersion)))
 		Expect(nodeClass.StatusConditions().IsTrue(v1alpha2.ConditionTypeNodeImagesReady)).To(BeTrue())
 	})
 
