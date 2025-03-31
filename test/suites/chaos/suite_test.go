5743a6ba3f189094e804a6349bfab2e3f3542d2f
tallaxes <18728999+tallaxes@users.noreply.github.com>
2025-03-31 07:22:33 +0000

    test: fix duplicate controller name on chaos testing

--- test/suites/chaos/suite_test.go
+++ test/suites/chaos/suite_test.go
@@ -31,6 +31,7 @@ import (
 	"k8s.io/client-go/tools/cache"
 	controllerruntime "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/controller"
 	"sigs.k8s.io/controller-runtime/pkg/manager"
 	"sigs.k8s.io/controller-runtime/pkg/predicate"
 	"sigs.k8s.io/controller-runtime/pkg/reconcile"
@@ -168,8 +169,7 @@ func (t *taintAdder) Reconcile(ctx context.Context, req reconcile.Request) (reco
 func (t *taintAdder) Builder(mgr manager.Manager) *controllerruntime.Builder {
 	return controllerruntime.NewControllerManagedBy(mgr).
 		For(&corev1.Node{}).
-		// TODO: restore after controller-runtime version update
-		// WithOptions(controller.Options{SkipNameValidation: lo.ToPtr(true)}).
+		WithOptions(controller.Options{SkipNameValidation: lo.ToPtr(true)}).
 		WithEventFilter(predicate.NewPredicateFuncs(func(obj client.Object) bool {
 			node := obj.(*corev1.Node)
 			if _, ok := node.Labels[coretest.DiscoveryLabel]; !ok {
