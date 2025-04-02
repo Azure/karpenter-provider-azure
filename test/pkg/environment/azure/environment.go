b0ae40dd6efe473c59f972008603b45e27afed35
Charlie McBride <33269602+charliedmcb@users.noreply.github.com>
2025-04-02 23:00:19 +0000

    add Kubernetes Upgrade E2E test suite

--- test/pkg/environment/azure/environment.go
+++ test/pkg/environment/azure/environment.go
@@ -26,6 +26,7 @@ import (
 	karpv1 "sigs.k8s.io/karpenter/pkg/apis/v1"
 
 	"github.com/Azure/azure-sdk-for-go/sdk/azidentity"
+	containerservice "github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/containerservice/armcontainerservice/v4"
 	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/network/armnetwork"
 	"github.com/Azure/karpenter-provider-azure/pkg/apis/v1alpha2"
 	"github.com/Azure/karpenter-provider-azure/pkg/test"
@@ -53,8 +54,9 @@ type Environment struct {
 	ClusterName          string
 	ClusterResourceGroup string
 
-	VNETClient       *armnetwork.VirtualNetworksClient
-	InterfacesClient *armnetwork.InterfacesClient
+	VNETClient              *armnetwork.VirtualNetworksClient
+	InterfacesClient        *armnetwork.InterfacesClient
+	AKSManagedClusterClient *containerservice.ManagedClustersClient
 }
 
 func NewEnvironment(t *testing.T) *Environment {
@@ -63,7 +65,7 @@ func NewEnvironment(t *testing.T) *Environment {
 		SubscriptionID:       lo.Must(os.LookupEnv("AZURE_SUBSCRIPTION_ID")),
 		ClusterName:          lo.Must(os.LookupEnv("AZURE_CLUSTER_NAME")),
 		ClusterResourceGroup: lo.Must(os.LookupEnv("AZURE_RESOURCE_GROUP")),
-		ACRName:              lo.Must(os.LookupEnv("ACR_NAME")),
+		ACRName:              lo.Must(os.LookupEnv("AZURE_ACR_NAME")),
 		Region:               lo.Ternary(os.Getenv("AZURE_LOCATION") == "", "westus2", os.Getenv("AZURE_LOCATION")),
 	}
 
@@ -74,6 +76,7 @@ func NewEnvironment(t *testing.T) *Environment {
 	cred := lo.Must(azidentity.NewDefaultAzureCredential(nil))
 	azureEnv.VNETClient = lo.Must(armnetwork.NewVirtualNetworksClient(azureEnv.SubscriptionID, cred, nil))
 	azureEnv.InterfacesClient = lo.Must(armnetwork.NewInterfacesClient(azureEnv.SubscriptionID, cred, nil))
+	azureEnv.AKSManagedClusterClient = lo.Must(containerservice.NewManagedClustersClient(azureEnv.SubscriptionID, cred, nil))
 	return azureEnv
 }
 
