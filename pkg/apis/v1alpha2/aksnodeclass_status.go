8d3617a536cdfd087814fc4a8100d1d4eedc972e
Charlie McBride <33269602+charliedmcb@users.noreply.github.com>
2025-04-18 13:17:21 -0700

    Update pkg/apis/v1alpha2/aksnodeclass_status.go
    Co-authored-by: Alex Leites <18728999+tallaxes@users.noreply.github.com>

--- pkg/apis/v1alpha2/aksnodeclass_status.go
+++ pkg/apis/v1alpha2/aksnodeclass_status.go
@@ -29,7 +29,7 @@ const (
 // NodeImage contains resolved image selector values utilized for node launch
 type NodeImage struct {
 	// The ID of the image. Examples:
-	// - CIG /CommunityGalleries/AKSUbuntu-38d80f77-467a-481f-a8d4-09b6d4220bd2/images/2204gen2containerd/versions/2022.10.03
+	// - CIG: /CommunityGalleries/AKSUbuntu-38d80f77-467a-481f-a8d4-09b6d4220bd2/images/2204gen2containerd/versions/2022.10.03
 	// - SIG: /subscriptions/10945678-1234-1234-1234-123456789012/resourceGroups/AKS-Ubuntu/providers/Microsoft.Compute/galleries/AKSUbuntu/images/2204gen2containerd/versions/2022.10.03
 	// +required
 	ID string `json:"id"`
