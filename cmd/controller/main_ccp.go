c602c65b6d41a065f3659ea16a7f87d96a5cb203
Alex Leites <18728999+tallaxes@users.noreply.github.com>
2025-04-18 17:07:14 -0700

    chore: update sigs.k8s.io/karpenter to v1.4.0 (#601)
    * chore: update sigs.k8s.io/karpenter to v1.1.0
    
    * chore: update main and alt operator
    
    * chore: remove webhooks wiring
    
    * chore: update operator
    
    * chore: update cloudprovider
    
    * chore: fix tests and update expectation helpers
    
    * chore: add repair policy test
    
    * chore: add node repair feature gate
    
    * chore: fix install-deps action and dependabot config
    
    * chore: remove go toolchain
    
    * chore: fix ci
    
    * chore: update Makefile
    
    * fix: skip debug controller name validation
    
    * fix: skip debug controller name validation
    
    * chore: remove comment
    
    * chore: update sig.k8s.io/karpenter to v1.1.1
    
    * chore: update comment
    
    * fix: allow nodeclass label
    
    * chore: regen CRDs
    
    * fix: spacing
    
    * chore: update sigs.k8s.io/karpenter to v1.3.2
    
    * fix: have Delete return NewNodeClaimNotFoundError if VM is deleted
    
    * test: fix duplicate controller name on chaos testing
    
    * chore: fix indent
    
    * feat: add Unknown Kubelet Ready condition to repair policies
    
    * fix: remove alt operator
    
    * fix: ensure NodeRepair feature gate is enabled for testing
    
    * chore: regenerate CRDs
    
    * chorre: regenerate CRDs
    
    * test: re-enable "drain pods in order" test
    
    * deps: bump sigs.k8s.io/karpenter to v1.4.0

--- cmd/controller/main_ccp.go
+++ cmd/controller/main_ccp.go
@@ -19,58 +19,20 @@ limitations under the License.
 package main
 
 import (
-	"context"
-
 	"github.com/samber/lo"
-	"go.uber.org/zap"
-	"knative.dev/pkg/logging"
 
-	// Injection stuff
-	kubeclientinjection "knative.dev/pkg/client/injection/kube/client"
-	"knative.dev/pkg/configmap"
-	"knative.dev/pkg/controller"
-	knativeinjection "knative.dev/pkg/injection"
-	secretinformer "knative.dev/pkg/injection/clients/namespacedkube/informers/core/v1/secret"
-	kubeinformerfactory "knative.dev/pkg/injection/clients/namespacedkube/informers/factory"
-	"knative.dev/pkg/webhook/certificates"
-
-	altOperator "github.com/Azure/karpenter-provider-azure/pkg/alt/karpenter-core/pkg/operator"
-	altwebhooks "github.com/Azure/karpenter-provider-azure/pkg/alt/karpenter-core/pkg/webhooks"
 	"github.com/Azure/karpenter-provider-azure/pkg/cloudprovider"
-	controllers "github.com/Azure/karpenter-provider-azure/pkg/controllers"
+	"github.com/Azure/karpenter-provider-azure/pkg/controllers"
 	"github.com/Azure/karpenter-provider-azure/pkg/operator"
+
 	"sigs.k8s.io/karpenter/pkg/cloudprovider/metrics"
 	corecontrollers "sigs.k8s.io/karpenter/pkg/controllers"
+	"sigs.k8s.io/karpenter/pkg/controllers/state"
+	coreoperator "sigs.k8s.io/karpenter/pkg/operator"
 )
 
-func newWebhooks(ctx context.Context) []knativeinjection.ControllerConstructor {
-	client := altOperator.GetCCPClient(ctx)
-	ccpInformerFactory := kubeinformerfactory.Get(ctx)
-
-	secretInformer := ccpInformerFactory.Core().V1().Secrets()
-	ctx = context.WithValue(ctx, secretinformer.Key{}, secretInformer)
-
-	logging.FromContext(ctx).Info("Starting horrible CCP informer")
-	if err := controller.StartInformers(ctx.Done(), secretInformer.Informer()); err != nil {
-		logging.FromContext(ctx).Fatalw("Failed to start horrible CCP informer", zap.Error(err))
-	}
-
-	return []knativeinjection.ControllerConstructor{
-		func(ctx context.Context, watcher configmap.Watcher) *controller.Impl {
-			ctx = context.WithValue(ctx, secretinformer.Key{}, secretInformer)
-			ctx = context.WithValue(ctx, kubeclientinjection.Key{}, client)
-			return certificates.NewController(ctx, watcher)
-		},
-		func(ctx context.Context, watcher configmap.Watcher) *controller.Impl {
-			ctx = context.WithValue(ctx, secretinformer.Key{}, secretInformer)
-			return altwebhooks.NewCRDConversionWebhook(ctx, watcher)
-		},
-	}
-}
-
 func main() {
-	//ctx, op := operator.NewOperator(coreoperator.NewOperator())
-	ctx, op := operator.NewOperator(altOperator.NewOperator())
+	ctx, op := operator.NewOperator(coreoperator.NewOperator())
 	aksCloudProvider := cloudprovider.New(
 		op.InstanceTypesProvider,
 		op.InstanceProvider,
@@ -81,6 +43,7 @@ func main() {
 
 	lo.Must0(op.AddHealthzCheck("cloud-provider", aksCloudProvider.LivenessProbe))
 	cloudProvider := metrics.Decorate(aksCloudProvider)
+	clusterState := state.NewCluster(op.Clock, op.GetClient(), cloudProvider)
 
 	op.
 		WithControllers(ctx, corecontrollers.NewControllers(
@@ -90,8 +53,8 @@ func main() {
 			op.GetClient(),
 			op.EventRecorder,
 			cloudProvider,
+			clusterState,
 		)...).
-		WithWebhooks(ctx, newWebhooks(ctx)...).
 		WithControllers(ctx, controllers.NewControllers(
 			ctx,
 			op.Manager,
@@ -102,5 +65,5 @@ func main() {
 			op.ImageProvider,
 			op.ImageProvider,
 		)...).
-		Start(ctx, cloudProvider)
+		Start(ctx)
 }
