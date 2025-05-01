f9b251a0ef5c5061fe388b0286ff7960f14f350b
Bryce Soghigian <bsoghigian@gmail.com>
2025-04-30 17:07:01 -0700

    fix: testing duration in e2e

--- test/suites/integration/repair_policy_test.go
+++ test/suites/integration/repair_policy_test.go
@@ -47,7 +47,7 @@ var _ = Describe("Repair Policy", func() {
 		unhealthyCondition = corev1.NodeCondition{
 			Type:               corev1.NodeReady,
 			Status:             corev1.ConditionFalse,
-			LastTransitionTime: metav1.Time{Time: time.Now().Add(-31 * time.Minute)},
+			LastTransitionTime: metav1.Time{Time: time.Now().Add(-11 * time.Minute)},
 		}
 		numPods = 1
 		// Add pods with a do-not-disrupt annotation so that we can check node metadata before we disrupt
