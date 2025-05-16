a4895e8e80469de0cee1f298bba7a7186ee9c490
Youn Jae Kim <younjaekim@microsoft.com>
2025-05-16 01:47:40 +0000

    Truncating error messages

--- pkg/cloudprovider/cloudprovider.go
+++ pkg/cloudprovider/cloudprovider.go
@@ -115,7 +115,7 @@ func (c *CloudProvider) Create(ctx context.Context, nodeClaim *karpv1.NodeClaim)
 		return nil, cloudprovider.NewNodeClassNotReadyError(stderrors.New(nodeClassReady.Message))
 	}
 	if nodeClassReady.IsUnknown() {
-		return nil, cloudprovider.NewCreateError(fmt.Errorf("resolving NodeClass readiness, NodeClass is in Ready=Unknown, %s", nodeClassReady.Message), NodeClassReadinessUnknownReason, "NodeClass is in Ready=Unknown")
+		return nil, cloudprovider.NewCreateError(fmt.Errorf("resolving NodeClass readiness, NodeClass is in Ready=Unknown, %s", truncateMessage(nodeClassReady.Message)), NodeClassReadinessUnknownReason, "NodeClass is in Ready=Unknown")
 	}
 	// Note: we make a call for GetKubernetesVersion here, as it has an internal check for the kubernetes version readiness KubernetesVersionReady,
 	//     where we don't want to proceed if it is unready.
@@ -130,14 +130,14 @@ func (c *CloudProvider) Create(ctx context.Context, nodeClaim *karpv1.NodeClaim)
 
 	instanceTypes, err := c.resolveInstanceTypes(ctx, nodeClaim, nodeClass)
 	if err != nil {
-		return nil, cloudprovider.NewCreateError(fmt.Errorf("resolving instance types, %w", err), InstanceTypeResolutionFailedReason, err.Error())
+		return nil, cloudprovider.NewCreateError(fmt.Errorf("resolving instance types, %s", truncateMessage(err.Error())), InstanceTypeResolutionFailedReason, err.Error())
 	}
 	if len(instanceTypes) == 0 {
 		return nil, cloudprovider.NewInsufficientCapacityError(fmt.Errorf("all requested instance types were unavailable during launch"))
 	}
 	instancePromise, err := c.instanceProvider.BeginCreate(ctx, nodeClass, nodeClaim, instanceTypes)
 	if err != nil {
-		return nil, cloudprovider.NewCreateError(fmt.Errorf("creating instance failed, %w", err), CreateInstanceFailedReason, err.Error())
+		return nil, cloudprovider.NewCreateError(fmt.Errorf("creating instance failed, %s", truncateMessage(err.Error())), CreateInstanceFailedReason, err.Error())
 	}
 
 	// Launch a single goroutine to poll the returned promise.
@@ -483,4 +483,13 @@ func newTerminatingNodeClassError(name string) *errors.StatusError {
 	err := errors.NewNotFound(qualifiedResource, name)
 	err.ErrStatus.Message = fmt.Sprintf("%s %q is terminating, treating as not found", qualifiedResource.String(), name)
 	return err
+}
+
+const truncateAt = 1200
+
+func truncateMessage(msg string) string {
+	if len(msg) < truncateAt {
+		return msg
+	}
+	return msg[:truncateAt] + "..."
 }
