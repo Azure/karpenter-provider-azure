c40dfe8add9fcebca511ffb465edce114bc53f92
Nandor Kollar <kollar.nandor@gmail.com>
2025-06-01 20:07:22 +0200

    add nolint: gocyclo

--- pkg/providers/imagefamily/bootstrap/aksbootstrap.go
+++ pkg/providers/imagefamily/bootstrap/aksbootstrap.go
@@ -372,6 +372,7 @@ func getCustomDataFromNodeBootstrapVars(nbv *NodeBootstrapVariables) (string, er
 	return buffer.String(), nil
 }
 
+// nolint: gocyclo
 func KubeletConfigToMap(kubeletConfig *KubeletConfiguration) map[string]string {
 	args := make(map[string]string)
 
