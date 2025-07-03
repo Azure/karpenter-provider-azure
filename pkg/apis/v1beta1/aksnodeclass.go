e3bcf46aaf0a0d8df1b0b8f6c0bbb9bcb6edd509
lnguyen924 <146486167+lnguyen924@users.noreply.github.com>
2025-07-03 22:24:44 +0000

    added validation to reject the unsupported combination of imageFamily: Ubuntu2204 and fipsMode: FIPS

--- pkg/apis/v1beta1/aksnodeclass.go
+++ pkg/apis/v1beta1/aksnodeclass.go
@@ -26,6 +26,7 @@ import (
 
 // AKSNodeClassSpec is the top level specification for the AKS Karpenter Provider.
 // This will contain configuration necessary to launch instances in AKS.
+// +kubebuilder:validation:XValidation:message="FIPS is not yet supported for Ubuntu2204",rule="has(self.imageFamily) && has(self.fipsMode) ?  !(self.imageFamily == 'Ubuntu2204' && self.fipsMode == 'FIPS') : true"
 type AKSNodeClassSpec struct {
 	// VNETSubnetID is the subnet used by nics provisioned with this nodeclass.
 	// If not specified, we will use the default --vnet-subnet-id specified in karpenter's options config
