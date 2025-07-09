cbeb6dc65ba9b3d1fa43fef0350f7613a9b9de10
lnguyen924 <146486167+lnguyen924@users.noreply.github.com>
2025-07-09 17:21:35 +0000

    made FIPS settings constants and added unit tests that validate the CEL

--- pkg/apis/v1beta1/crd_validation_cel_test.go
+++ pkg/apis/v1beta1/crd_validation_cel_test.go
@@ -94,6 +94,51 @@ var _ = Describe("CEL/Validation", func() {
 			Entry("invalid resource group name with unsupported chars 'name@with*unsupported&chars'", "/subscriptions/12345678-1234-1234-1234-123456789012/resourceGroups/name@with*unsupported&chars/providers/Microsoft.Network/virtualNetworks/vnet/subnets/subnet", false),
 		)
 	})
+
+	Context("ImageFamily and FIPSMode", func() {
+		DescribeTable("should only accept valid ImageFamily and FIPSMode combinations", func(imageFamily string, fipsMode string, expected bool) {
+			nodeClass := &v1beta1.AKSNodeClass{
+				ObjectMeta: metav1.ObjectMeta{Name: strings.ToLower(randomdata.SillyName())},
+				Spec:       v1beta1.AKSNodeClassSpec{},
+			}
+			// allows for leaving imageFamily unset, which currently defaults to Ubuntu2204
+			if imageFamily != "" {
+				nodeClass.Spec.ImageFamily = &imageFamily
+			}
+			// allows for leaving fipsMode unset, which currently effectively behaves like setting fipsMode to Disabled
+			if fipsMode != v1beta1.FIPSUnspecified {
+				nodeClass.Spec.FIPSMode = &fipsMode
+			}
+			if expected {
+				Expect(env.Client.Create(ctx, nodeClass)).To(Succeed())
+			} else {
+				Expect(env.Client.Create(ctx, nodeClass)).ToNot(Succeed())
+			}
+		},
+			Entry("generic Ubuntu when FIPSMode is explicitly Disabled should succeed", "Ubuntu", v1beta1.FIPSDisabled, true),
+			Entry("generic Ubuntu when FIPSMode is not explicitly set should succeed", "Ubuntu", v1beta1.FIPSUnspecified, true),
+			Entry("generic Ubuntu when FIPSMode is explicitly FIPS should succeed", "Ubuntu", v1beta1.FIPSEnabled, true),
+			Entry("Ubuntu2204 when FIPSMode is explicitly Disabled should succeed", "Ubuntu2204", v1beta1.FIPSDisabled, true),
+			Entry("Ubuntu2204 when FIPSMode is not explicitly set should succeed", "Ubuntu2204", v1beta1.FIPSUnspecified, true),
+			//TODO: Modify when Ubuntu 22.04 with FIPS becomes available
+			Entry("Ubuntu2204 when FIPSMode is explicitly FIPS should fail", "Ubuntu2204", v1beta1.FIPSEnabled, false),
+			Entry("generic AzureLinux when FIPSMode is explicitly Disabled should succeed", "AzureLinux", v1beta1.FIPSDisabled, true),
+			Entry("generic AzureLinux when FIPSMode is not explicitly set should succeed", "AzureLinux", v1beta1.FIPSUnspecified, true),
+			Entry("generic AzureLinux when FIPSMode is explicitly FIPS should succeed", "AzureLinux", v1beta1.FIPSEnabled, true),
+			Entry("unspecified ImageFamily (defaults to Ubuntu2204) when FIPSMode is explicitly Disabled should succeed", "", v1beta1.FIPSDisabled, true),
+			Entry("unspecified ImageFamily (defaults to Ubuntu2204) when FIPSMode is not explicitly set should succeed", "", v1beta1.FIPSUnspecified, true),
+			Entry("unspecified ImageFamily (defaults to Ubuntu2204) when FIPSMode is explicitly FIPS should fail", "", v1beta1.FIPSEnabled, false),
+			Entry("invalid ImageFamily when FIPSMode is explicitly Disabled should fail", "abc", v1beta1.FIPSDisabled, false),
+			Entry("invalid ImageFamily when FIPSMode is not explicitly set should fail", "abc", v1beta1.FIPSUnspecified, false),
+			Entry("invalid ImageFamily when FIPSMode is explicitly FIPS should fail", "abc", v1beta1.FIPSEnabled, false),
+			Entry("invalid FIPSMode when ImageFamily is explicitly Ubuntu should fail", "Ubuntu", "xyz", false),
+			Entry("invalid FIPSMode when ImageFamily is explicitly Ubuntu2204 should fail", "Ubuntu2204", "xyz", false),
+			Entry("invalid FIPSMode when ImageFamily is explicitly AzureLinux should fail", "AzureLinux", "xyz", false),
+			Entry("invalid FIPSMode when ImageFamily is not explicitly set (defaults to Ubuntu2204) should fail", "", "xyz", false),
+			Entry("invalid FIPSMode and invalid ImageFamily should fail", "abc", "xyz", false),
+		)
+	})
+
 	Context("Requirements", func() {
 		It("should allow restricted domains exceptions", func() {
 			oldNodePool := nodePool.DeepCopy()
