262efcde2b1e48f62cc0e61f7e1dd002b00d6cd3
Bryce Soghigian <bsoghigian@gmail.com>
2025-09-12 02:25:04 -0700

    fix: shouldSkip

--- test/suites/utilization/suite_test.go
+++ test/suites/utilization/suite_test.go
@@ -70,12 +70,12 @@ var _ = Describe("Utilization", func() {
 		// Entry("AzureLinux, arm64", env.AZLinuxNodeClass().Spec.ImageFamily, karpv1.ArchitectureArm64,
 
 		// Ubuntu
-		Entry("Ubuntu, amd64", lo.ToPtr(v1beta1.UbuntuImageFamily), karpv1.ArchitectureAmd64, false),
-		Entry("Ubuntu, arm64", lo.ToPtr(v1beta1.UbuntuImageFamily), karpv1.ArchitectureArm64, false),
-		Entry("Ubuntu2204, amd64", lo.ToPtr(v1beta1.Ubuntu2204ImageFamily), karpv1.ArchitectureAmd64, false),
-		Entry("Ubuntu2204, arm64", lo.ToPtr(v1beta1.Ubuntu2204ImageFamily), karpv1.ArchitectureArm64, false),
-		Entry("Ubuntu2404, amd64", lo.ToPtr(v1beta1.Ubuntu2404ImageFamily), karpv1.ArchitectureAmd64, false),
-		Entry("Ubuntu2404, arm64", lo.ToPtr(v1beta1.Ubuntu2404ImageFamily), karpv1.ArchitectureArm64, false),
+		Entry("Ubuntu, amd64", lo.ToPtr(v1beta1.UbuntuImageFamily), karpv1.ArchitectureAmd64),
+		Entry("Ubuntu, arm64", lo.ToPtr(v1beta1.UbuntuImageFamily), karpv1.ArchitectureArm64),
+		Entry("Ubuntu2204, amd64", lo.ToPtr(v1beta1.Ubuntu2204ImageFamily), karpv1.ArchitectureAmd64),
+		Entry("Ubuntu2204, arm64", lo.ToPtr(v1beta1.Ubuntu2204ImageFamily), karpv1.ArchitectureArm64),
+		Entry("Ubuntu2404, amd64", lo.ToPtr(v1beta1.Ubuntu2404ImageFamily), karpv1.ArchitectureAmd64),
+		Entry("Ubuntu2404, arm64", lo.ToPtr(v1beta1.Ubuntu2404ImageFamily), karpv1.ArchitectureArm64),
 	)
 
 	It("should provision one pod per node (AzureLinux, arm64)", func() {
