05ad48f1a9a75b8de48099d48b0e1e230e58a5a1
Charlie McBride <33269602+charliedmcb@users.noreply.github.com>
2025-09-17 19:24:57 +0000

    fix end of file

--- hack/validation/taints.sh
+++ hack/validation/taints.sh
@@ -5,17 +5,10 @@ set -euo pipefail
 # checking for restricted taints in kubernetes.azure.com domain
 
 # Rule for startupTaints: completely block kubernetes.azure.com domain
-startupTaintsRule=$'self.all(x, !x.key.find("^([^/]+)").endsWith("kubernetes.azure.com"))'
+startupTaintsRule='!self.startsWith("kubernetes.azure.com")'
 
-# Rule for taints: allow only specific kubernetes.azure.com keys
-taintsRule=$'self.all(x, x.key.find("^([^/]+)").endsWith("kubernetes.azure.com") ?
-    x.key in [
-        "kubernetes.azure.com/scalesetpriority",
-        "kubernetes.azure.com/mode"
-    ]
-    : true
-)
-'
+# Rule for taints: allow only specific kubernetes.azure.com keys (negated logic for CEL validation)
+taintsRule='!(self.startsWith("kubernetes.azure.com") && self != "kubernetes.azure.com/scalesetpriority" && self != "kubernetes.azure.com/mode")'
 
 # Escape quotes and clean up the rules
 startupTaintsRule=${startupTaintsRule//\"/\\\"}
@@ -29,12 +22,15 @@ taintsRule=$(echo "$taintsRule" | tr -s ' ')
 # check that .spec.versions has 1 entry
 [[ $(yq e '.spec.versions | length' pkg/apis/crds/karpenter.sh_nodepools.yaml) -eq 1 ]] || { echo "expected one version"; exit 1; }
 
-# Add validation for startupTaints
-printf -v startupTaintsExpr '.spec.versions[0].schema.openAPIV3Schema.properties.spec.properties.template.properties.spec.properties.startupTaints.x-kubernetes-validations +=
+# Add validation for startupTaints key field
+printf -v startupTaintsExpr '.spec.versions[0].schema.openAPIV3Schema.properties.spec.properties.template.properties.spec.properties.startupTaints.items.properties.key.x-kubernetes-validations +=
     [{"message": "taint domain \\"kubernetes.azure.com\\" is restricted", "rule": "%s"}]' "$startupTaintsRule"
 yq eval "${startupTaintsExpr}" -i pkg/apis/crds/karpenter.sh_nodepools.yaml
 
-# Add validation for taints
-printf -v taintsExpr '.spec.versions[0].schema.openAPIV3Schema.properties.spec.properties.template.properties.spec.properties.taints.x-kubernetes-validations +=
+# Add validation for taints key field
+printf -v taintsExpr '.spec.versions[0].schema.openAPIV3Schema.properties.spec.properties.template.properties.spec.properties.taints.items.properties.key.x-kubernetes-validations +=
     [{"message": "taint domain \\"kubernetes.azure.com\\" is restricted", "rule": "%s"}]' "$taintsRule"
 yq eval "${taintsExpr}" -i pkg/apis/crds/karpenter.sh_nodepools.yaml
+
+# Set maxItems limit for taints array (required for CEL validation time complexity limits)
+yq eval '.spec.versions[0].schema.openAPIV3Schema.properties.spec.properties.template.properties.spec.properties.taints.maxItems = 100' -i pkg/apis/crds/karpenter.sh_nodepools.yaml
