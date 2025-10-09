c9efe33f1d5cf5e2639b989f12e8d4e0f3f7f379
Charlie McBride <33269602+charliedmcb@users.noreply.github.com>
2025-10-09 21:04:55 +0000

    add machines tests

--- test/pkg/environment/azure/identity.go
+++ test/pkg/environment/azure/identity.go
@@ -18,12 +18,16 @@ package azure
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/Azure/azure-sdk-for-go/sdk/azcore"
 	"github.com/Azure/azure-sdk-for-go/sdk/azcore/policy"
+	"github.com/Azure/azure-sdk-for-go/sdk/azcore/runtime"
+	"github.com/Azure/azure-sdk-for-go/sdk/azcore/to"
 	containerservice "github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/containerservice/armcontainerservice/v8"
 	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/msi/armmsi"
 	"github.com/golang-jwt/jwt/v5"
+	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 	"github.com/samber/lo"
 )
@@ -64,4 +68,99 @@ func (env *Environment) GetCurrentUserPrincipalID(ctx context.Context, cred azco
 	Expect(oid).ToNot(BeEmpty(), "oid claim is empty")
 
 	return oid
+}
+
+// ExpectCreatedManagedIdentity creates a new user-assigned managed identity
+func (env *Environment) ExpectCreatedManagedIdentity(ctx context.Context, identityName string) *armmsi.Identity {
+	GinkgoHelper()
+	msiClient, err := armmsi.NewUserAssignedIdentitiesClient(env.SubscriptionID, env.GetDefaultCredential(), nil)
+	Expect(err).ToNot(HaveOccurred())
+
+	By(fmt.Sprintf("creating managed identity %s in node resource group %s", identityName, env.NodeResourceGroup))
+
+	identity := armmsi.Identity{
+		Location: to.Ptr(env.Region),
+		Tags: map[string]*string{
+			"test": to.Ptr("karpenter-e2e"),
+		},
+	}
+
+	resp, err := msiClient.CreateOrUpdate(ctx, env.NodeResourceGroup, identityName, identity, nil)
+	Expect(err).ToNot(HaveOccurred())
+
+	// Note: we don't register for cleanup in the env.tracker, in case there are more tests to run. We don't want to break the cluster by deleting the kubelet identity
+	return &resp.Identity
+}
+
+// ExpectUpdatedManagedClusterKubeletIdentityAsync updates the kubelet identity of a managed cluster asynchronously
+// Returns the poller so the caller can control when to wait for completion
+func (env *Environment) ExpectUpdatedManagedClusterKubeletIdentityAsync(ctx context.Context, newIdentity *armmsi.Identity) *runtime.Poller[containerservice.ManagedClustersClientCreateOrUpdateResponse] {
+	GinkgoHelper()
+
+	By("getting current managed cluster configuration")
+	mc := env.ExpectGetManagedCluster()
+
+	By("updating kubelet identity")
+
+	// Update the kubelet identity in the identity profile
+	if mc.Properties.IdentityProfile == nil {
+		mc.Properties.IdentityProfile = make(map[string]*containerservice.UserAssignedIdentity)
+	}
+
+	mc.Properties.IdentityProfile["kubeletidentity"] = &containerservice.UserAssignedIdentity{
+		ClientID:   newIdentity.Properties.ClientID,
+		ObjectID:   newIdentity.Properties.PrincipalID,
+		ResourceID: newIdentity.ID,
+	}
+
+	// Update the cluster and return the poller
+	poller, err := env.managedClusterClient.BeginCreateOrUpdate(ctx, env.ClusterResourceGroup, env.ClusterName, *mc, nil)
+	Expect(err).ToNot(HaveOccurred())
+
+	return poller
+}
+
+// ExpectGrantedACRAccess grants the specified identity access to pull from the ACR
+func (env *Environment) ExpectGrantedACRAccess(ctx context.Context, identity *armmsi.Identity) {
+	GinkgoHelper()
+
+	By("granting ACR pull access to identity")
+
+	// Get the ACR resource ID
+	acrResourceID := fmt.Sprintf("/subscriptions/%s/resourceGroups/%s/providers/Microsoft.ContainerRegistry/registries/%s",
+		env.SubscriptionID, env.ClusterResourceGroup, env.ACRName)
+
+	// AcrPull role definition ID: 7f951dda-4ed3-4680-a7ca-43fe172d538d
+	acrPullRoleID := fmt.Sprintf("/subscriptions/%s/providers/Microsoft.Authorization/roleDefinitions/7f951dda-4ed3-4680-a7ca-43fe172d538d", env.SubscriptionID)
+
+	identityPrincipalID := lo.FromPtr(identity.Properties.PrincipalID)
+
+	err := env.RBACManager.EnsureRoleWithPrincipalType(ctx, acrResourceID, acrPullRoleID, identityPrincipalID, "ServicePrincipal")
+	Expect(err).ToNot(HaveOccurred())
+}
+
+// CheckClusterIdentityType returns the type of managed identity used by the cluster
+func (env *Environment) CheckClusterIdentityType(ctx context.Context) string {
+	mc := env.ExpectGetManagedCluster()
+	if mc.Identity == nil {
+		return "none"
+	}
+	if mc.Identity.Type == nil {
+		return "unknown"
+	}
+	return string(*mc.Identity.Type)
+}
+
+// IsClusterUserAssignedIdentity checks if the cluster uses user-assigned managed identity
+func (env *Environment) IsClusterUserAssignedIdentity(ctx context.Context) bool {
+	identityType := env.CheckClusterIdentityType(ctx)
+	return identityType == "UserAssigned"
+}
+
+// GetKubeletIdentity returns the current kubelet identity
+func (env *Environment) GetKubeletIdentity(ctx context.Context) *containerservice.UserAssignedIdentity {
+	mc := env.ExpectGetManagedCluster()
+	Expect(mc.Properties.IdentityProfile).ToNot(BeNil())
+	Expect(mc.Properties.IdentityProfile["kubeletidentity"]).ToNot(BeNil())
+	return mc.Properties.IdentityProfile["kubeletidentity"]
 }
