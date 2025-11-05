24fcff7096de72ac4f236664f315be0bf3408920
Saewon Kwak <23280628+saewoni@users.noreply.github.com>
2025-11-05 19:34:52 +0000

    move the localdns test back to test/suites/integration

--- test/suites/integration/localdns_test.go
+++ test/suites/integration/localdns_test.go
@@ -14,7 +14,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-package localdns_test
+package integration_test
 
 import (
 	"github.com/Azure/karpenter-provider-azure/pkg/apis/v1beta1"
