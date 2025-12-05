92c72efa4764264926235ba28a3a1b6b19977494
Robin Deeboonchai <rdeeboonchai@microsoft.com>
2025-12-04 17:22:16 -0800

    chore: update import to v8

--- pkg/fake/aksagentpoolsapi.go
+++ pkg/fake/aksagentpoolsapi.go
@@ -25,7 +25,7 @@ import (
 
 	"github.com/Azure/azure-sdk-for-go/sdk/azcore"
 	"github.com/Azure/azure-sdk-for-go/sdk/azcore/runtime"
-	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/containerservice/armcontainerservice/v7"
+	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/containerservice/armcontainerservice/v8"
 	"github.com/Azure/karpenter-provider-azure/pkg/providers/instance"
 )
 
