ab02e85d874cd8d4c06c3642336e9ab1bb30acab
Robin Deeboonchai <rdeeboonchai@microsoft.com>
2025-12-29 12:15:20 -0800

    test: fix tests to be compatible with custom VNet

--- test/pkg/environment/azure/expectations.go
+++ test/pkg/environment/azure/expectations.go
@@ -107,35 +107,15 @@ func (env *Environment) ExpectParsedProviderID(providerID string) string {
 	return providerIDSplit[len(providerIDSplit)-1]
 }
 
-func (env *Environment) ExpectCreatedVNet(vnet *armnetwork.VirtualNetwork) {
-	GinkgoHelper()
-	poller, err := env.vnetClient.BeginCreateOrUpdate(env.Context, env.NodeResourceGroup, lo.FromPtr(vnet.Name), *vnet, nil)
-	Expect(err).ToNot(HaveOccurred())
-	resp, err := poller.PollUntilDone(env.Context, nil)
-	Expect(err).ToNot(HaveOccurred())
-	*vnet = resp.VirtualNetwork
-	env.tracker.Add(lo.FromPtr(resp.ID), func() error {
-		deletePoller, err := env.vnetClient.BeginDelete(env.Context, env.NodeResourceGroup, lo.FromPtr(vnet.Name), nil)
-		if err != nil {
-			return fmt.Errorf("failed to delete vnet %s: %w", lo.FromPtr(vnet.Name), err)
-		}
-		_, err = deletePoller.PollUntilDone(env.Context, nil)
-		if err != nil {
-			return fmt.Errorf("failed to delete vnet %s: %w", lo.FromPtr(vnet.Name), err)
-		}
-		return nil
-	})
-}
-
 func (env *Environment) ExpectCreatedSubnet(vnetName string, subnet *armnetwork.Subnet) {
 	GinkgoHelper()
-	poller, err := env.subnetClient.BeginCreateOrUpdate(env.Context, env.NodeResourceGroup, vnetName, lo.FromPtr(subnet.Name), *subnet, nil)
+	poller, err := env.subnetClient.BeginCreateOrUpdate(env.Context, env.VNETResourceGroup, vnetName, lo.FromPtr(subnet.Name), *subnet, nil)
 	Expect(err).ToNot(HaveOccurred())
 	resp, err := poller.PollUntilDone(env.Context, nil)
 	Expect(err).ToNot(HaveOccurred())
 	*subnet = resp.Subnet
 	env.tracker.Add(lo.FromPtr(resp.ID), func() error {
-		deletePoller, err := env.subnetClient.BeginDelete(env.Context, env.NodeResourceGroup, vnetName, lo.FromPtr(subnet.Name), nil)
+		deletePoller, err := env.subnetClient.BeginDelete(env.Context, env.VNETResourceGroup, vnetName, lo.FromPtr(subnet.Name), nil)
 		if err != nil {
 			return fmt.Errorf("failed to delete subnet %s: %w", lo.FromPtr(subnet.Name), err)
 		}
