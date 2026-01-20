b615a7ce6bd64d6da241186867a6a8794fb059ea
Bevan Arps <bevan.arps@microsoft.com>
2026-01-20 04:03:06 +0000

    Upgrade referenced version

--- hack/toolchain.sh
+++ hack/toolchain.sh
@@ -12,7 +12,7 @@ main() {
 
 tools() {
     go install github.com/google/go-licenses@v1.6.0
-    go install github.com/golangci/golangci-lint/cmd/golangci-lint@v1.64.8
+    go install github.com/golangci/golangci-lint/v2/cmd/golangci-lint@v2.8.0
     go install github.com/google/ko@v0.17.1
     go install github.com/mikefarah/yq/v4@v4.45.1
     go install github.com/norwoodj/helm-docs/cmd/helm-docs@v1.14.2
