24c40c7876de9b705e1a3794b5cafaf1ac1abad2
Matthew Christopher <matthchr@users.noreply.github.com>
2026-02-19 23:42:48 +0000

    feat: restrict kubernetes.azure.com labels
    * Restricts kubernetes.azure.com/ label namespace, except for
      WellKnownLabels (and a few special labels that aren't WellKnown but we
      allow).
    * Also restricts the agentpool, storageprofile, storagetier, and
      accelerator labels, matching what AKS RP restricts.

--- hack/validation/labels.sh
+++ hack/validation/labels.sh
@@ -30,10 +30,80 @@ rule=${rule//\"/\\\"}            # escape double quotes
 rule=${rule//$'\n'/}             # remove newlines
 rule=$(echo "$rule" | tr -s ' ') # remove extra spaces
 
+# kubernetes.azure.com domain restriction
+# We allow well-known labels as well as ebpf-dataplane (required for Cilium)
+aks_rule=$'self.all(x, x in
+    [
+        "kubernetes.azure.com/mode",
+        "kubernetes.azure.com/scalesetpriority",
+        "kubernetes.azure.com/priority",
+        "kubernetes.azure.com/fips_enabled",
+        "kubernetes.azure.com/os-sku",
+        "kubernetes.azure.com/cluster",
+        "kubernetes.azure.com/sku-cpu",
+        "kubernetes.azure.com/sku-memory",
+        "kubernetes.azure.com/ebpf-dataplane",
+    ]
+    || !x.find("^([^/]+)").endsWith("kubernetes.azure.com")
+)
+'
+
+aks_rule=${aks_rule//\"/\\\"}            # escape double quotes
+aks_rule=${aks_rule//$'\n'/}             # remove newlines
+aks_rule=$(echo "$aks_rule" | tr -s ' ') # remove extra spaces
+
+# agentpool restriction
+agentpool_rule=$'self.all(x, x != \x27agentpool\x27)'
+
+agentpool_rule=${agentpool_rule//\"/\\\"}            # escape double quotes
+agentpool_rule=${agentpool_rule//$'\n'/}             # remove newlines
+agentpool_rule=$(echo "$agentpool_rule" | tr -s ' ') # remove extra spaces
+
+# storageprofile restriction
+storageprofile_rule=$'self.all(x, x != \x27storageprofile\x27)'
+
+storageprofile_rule=${storageprofile_rule//\"/\\\"}            # escape double quotes
+storageprofile_rule=${storageprofile_rule//$'\n'/}             # remove newlines
+storageprofile_rule=$(echo "$storageprofile_rule" | tr -s ' ') # remove extra spaces
+
+# storagetier restriction
+storagetier_rule=$'self.all(x, x != \x27storagetier\x27)'
+
+storagetier_rule=${storagetier_rule//\"/\\\"}            # escape double quotes
+storagetier_rule=${storagetier_rule//$'\n'/}             # remove newlines
+storagetier_rule=$(echo "$storagetier_rule" | tr -s ' ') # remove extra spaces
+
+# accelerator restriction
+accelerator_rule=$'self.all(x, x != \x27accelerator\x27)'
+
+accelerator_rule=${accelerator_rule//\"/\\\"}            # escape double quotes
+accelerator_rule=${accelerator_rule//$'\n'/}             # remove newlines
+accelerator_rule=$(echo "$accelerator_rule" | tr -s ' ') # remove extra spaces
+
 # check that .spec.versions has 1 entry
 [[ $(yq e '.spec.versions | length' pkg/apis/crds/karpenter.sh_nodepools.yaml) -eq 1 ]] || { echo "expected one version"; exit 1; }
 
 # nodepool
 printf -v expr '.spec.versions[0].schema.openAPIV3Schema.properties.spec.properties.template.properties.metadata.properties.labels.x-kubernetes-validations +=
     [{"message": "label domain \\"karpenter.azure.com\\" is restricted", "rule": "%s"}]' "$rule"
+yq eval "${expr}" -i pkg/apis/crds/karpenter.sh_nodepools.yaml
+
+printf -v expr '.spec.versions[0].schema.openAPIV3Schema.properties.spec.properties.template.properties.metadata.properties.labels.x-kubernetes-validations +=
+    [{"message": "label domain \\"kubernetes.azure.com\\" is restricted", "rule": "%s"}]' "$aks_rule"
+yq eval "${expr}" -i pkg/apis/crds/karpenter.sh_nodepools.yaml
+
+printf -v expr '.spec.versions[0].schema.openAPIV3Schema.properties.spec.properties.template.properties.metadata.properties.labels.x-kubernetes-validations +=
+    [{"message": "label \\"agentpool\\" is restricted", "rule": "%s"}]' "$agentpool_rule"
+yq eval "${expr}" -i pkg/apis/crds/karpenter.sh_nodepools.yaml
+
+printf -v expr '.spec.versions[0].schema.openAPIV3Schema.properties.spec.properties.template.properties.metadata.properties.labels.x-kubernetes-validations +=
+    [{"message": "label \\"storageprofile\\" is restricted", "rule": "%s"}]' "$storageprofile_rule"
+yq eval "${expr}" -i pkg/apis/crds/karpenter.sh_nodepools.yaml
+
+printf -v expr '.spec.versions[0].schema.openAPIV3Schema.properties.spec.properties.template.properties.metadata.properties.labels.x-kubernetes-validations +=
+    [{"message": "label \\"storagetier\\" is restricted", "rule": "%s"}]' "$storagetier_rule"
+yq eval "${expr}" -i pkg/apis/crds/karpenter.sh_nodepools.yaml
+
+printf -v expr '.spec.versions[0].schema.openAPIV3Schema.properties.spec.properties.template.properties.metadata.properties.labels.x-kubernetes-validations +=
+    [{"message": "label \\"accelerator\\" is restricted", "rule": "%s"}]' "$accelerator_rule"
 yq eval "${expr}" -i pkg/apis/crds/karpenter.sh_nodepools.yaml
