eee89c8c12749351bb58432b0a49c5a133573cd5
Robin Deeboonchai <rdeeboonchai@microsoft.com>
2026-04-01 16:14:57 -0700

    Revert "fix: add sku-hyperv-generation to CRD label allowlist"
    This reverts commit a6a6ac671ee21c574ce2e1cce45520f34d4c6768.

--- hack/validation/requirements.sh
+++ hack/validation/requirements.sh
@@ -18,8 +18,7 @@ rule=$'self in
         "karpenter.azure.com/sku-storage-ephemeralos-maxsize",
         "karpenter.azure.com/sku-gpu-name",
         "karpenter.azure.com/sku-gpu-manufacturer",
-        "karpenter.azure.com/sku-gpu-count",
-        "karpenter.azure.com/sku-hyperv-generation"
+        "karpenter.azure.com/sku-gpu-count"
     ]
     || !self.find("^([^/]+)").endsWith("karpenter.azure.com")
 '
