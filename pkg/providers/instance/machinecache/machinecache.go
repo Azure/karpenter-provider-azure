cfee415e73d097f0ee441d6e993c171240c98c08
Pablo Trivino <46615580+PabloTriv@users.noreply.github.com>
2026-04-14 23:28:15 +0000

    freshget

--- pkg/providers/instance/machinecache/machinecache.go
+++ pkg/providers/instance/machinecache/machinecache.go
@@ -292,6 +292,7 @@ func (c *MachineCache) run() {
 // update refreshes the machine cache by fetching the latest list of AKS machines from the Azure API.
 // This should NOT be called directly; it is intended to be used by the background worker.
 func (c *MachineCache) update(ctx context.Context) error {
+	fmt.Println("DEBUG: MachineCache.update called")
 	if c.isFresh() {
 		return nil
 	}
@@ -339,6 +340,7 @@ func (c *MachineCache) update(ctx context.Context) error {
 }
 
 func (c *MachineCache) isFresh() bool {
+	fmt.Println("DEBUG: MachineCache.isFresh called")
 	lastUpdatedNanos := c.lastUpdatedUnixNanos.Load()
 	if lastUpdatedNanos == 0 {
 		return false
