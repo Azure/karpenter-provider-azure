dfadd1de73fbf49ea65cbf8e06cdb3ec7ccaec2d
Pablo Trivino <pablotrivino@microsoft.com>
2026-04-30 16:32:20 -0700

    change to go native

--- pkg/utils/machine/machine_test.go
+++ pkg/utils/machine/machine_test.go
@@ -11,243 +11,202 @@ import (
 	"github.com/Azure/azure-sdk-for-go/sdk/azcore"
 	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/containerservice/armcontainerservice/v9"
 	"github.com/Azure/karpenter-provider-azure/pkg/consts"
-	"github.com/samber/lo"
-
-	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
+	"github.com/samber/lo"
 )
 
-func TestUtils(t *testing.T) {
-	RegisterFailHandler(Fail)
-	RunSpecs(t, "Utils Suite")
-}
+func TestHandleProvisioningState(t *testing.T) {
+	t.Parallel()
+	tests := []struct {
+		name                    string
+		provisioningState       string
+		provisioningError       *armcontainerservice.ErrorDetail
+		status                  *armcontainerservice.MachineStatus
+		expectedErrorDetails    *armcontainerservice.ErrorDetail
+		expectedPollingErr      bool
+		expectedPollingErrMsg   string
+		expectedDone            bool
+	}{
+		{
+			name:                  "Creating state - non-terminal",
+			provisioningState:     consts.ProvisioningStateCreating,
+			expectedErrorDetails:  nil,
+			expectedPollingErr:    false,
+			expectedDone:          false,
+		},
+		{
+			name:                  "Updating state - non-terminal",
+			provisioningState:     consts.ProvisioningStateUpdating,
+			expectedErrorDetails:  nil,
+			expectedPollingErr:    false,
+			expectedDone:          false,
+		},
+		{
+			name:                  "Succeeded state - terminal success",
+			provisioningState:     consts.ProvisioningStateSucceeded,
+			expectedErrorDetails:  nil,
+			expectedPollingErr:    false,
+			expectedDone:          true,
+		},
+		{
+			name:                  "Deleting state - terminal canceled",
+			provisioningState:     consts.ProvisioningStateDeleting,
+			expectedErrorDetails:  nil,
+			expectedPollingErr:    true,
+			expectedPollingErrMsg: "canceled provisioning state",
+			expectedDone:          true,
+		},
+		{
+			name:              "Failed state with ProvisioningError",
+			provisioningState: consts.ProvisioningStateFailed,
+			status: &armcontainerservice.MachineStatus{
+				ProvisioningError: &armcontainerservice.ErrorDetail{
+					Code:    lo.ToPtr("QuotaExceeded"),
+					Message: lo.ToPtr("Quota exceeded for VM family"),
+				},
+			},
+			expectedErrorDetails: &armcontainerservice.ErrorDetail{
+				Code:    lo.ToPtr("QuotaExceeded"),
+				Message: lo.ToPtr("Quota exceeded for VM family"),
+			},
+			expectedPollingErr: false,
+			expectedDone:       true,
+		},
+		{
+			name:              "Failed state with nil ProvisioningError",
+			provisioningState: consts.ProvisioningStateFailed,
+			status: &armcontainerservice.MachineStatus{
+				ProvisioningError: nil,
+			},
+			expectedErrorDetails:  nil,
+			expectedPollingErr:    true,
+			expectedPollingErrMsg: "ProvisioningError is nil",
+			expectedDone:          true,
+		},
+		{
+			name:                  "Failed state with nil Status",
+			provisioningState:     consts.ProvisioningStateFailed,
+			status:                nil,
+			expectedErrorDetails:  nil,
+			expectedPollingErr:    true,
+			expectedPollingErrMsg: "ProvisioningError is nil",
+			expectedDone:          true,
+		},
+		{
+			name:                  "Unknown state",
+			provisioningState:     "UnknownState",
+			expectedErrorDetails:  nil,
+			expectedPollingErr:    false,
+			expectedDone:          false,
+		},
+		{
+			name:                  "Empty state",
+			provisioningState:     "",
+			expectedErrorDetails:  nil,
+			expectedPollingErr:    false,
+			expectedDone:          false,
+		},
+	}
 
-var _ = Describe("Utils", func() {
-	Describe("HandleProvisioningState", func() {
-		var (
-			ctx        context.Context
-			aksMachine *armcontainerservice.Machine
-		)
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+			g := NewWithT(t)
+			ctx := context.Background()
 
-		BeforeEach(func() {
-			ctx = context.Background()
-			aksMachine = &armcontainerservice.Machine{
+			aksMachine := &armcontainerservice.Machine{
 				ID:   lo.ToPtr("test-machine-id"),
 				Name: lo.ToPtr("test-machine-name"),
 				Properties: &armcontainerservice.MachineProperties{
-					ProvisioningState: lo.ToPtr(consts.ProvisioningStateCreating),
+					ProvisioningState: lo.ToPtr(tt.provisioningState),
+					Status:            tt.status,
 				},
 			}
+
+			errorDetails, pollingErr, done := HandleProvisioningState(ctx, aksMachine)
+
+			g.Expect(errorDetails).To(Equal(tt.expectedErrorDetails))
+			if tt.expectedPollingErr {
+				g.Expect(pollingErr).To(HaveOccurred())
+				g.Expect(pollingErr.Error()).To(ContainSubstring(tt.expectedPollingErrMsg))
+			} else {
+				g.Expect(pollingErr).ToNot(HaveOccurred())
+			}
+			g.Expect(done).To(Equal(tt.expectedDone))
 		})
+	}
+}
 
-		Context("non-terminal states", func() {
-			It("should return done=false for Creating state", func() {
-				aksMachine.Properties.ProvisioningState = lo.ToPtr(consts.ProvisioningStateCreating)
+func TestIsAKSMachineOrMachinesPoolNotFound(t *testing.T) {
+	t.Parallel()
+	tests := []struct {
+		name     string
+		err      error
+		expected bool
+	}{
+		{
+			name:     "nil error",
+			err:      nil,
+			expected: false,
+		},
+		{
+			name:     "404 Not Found",
+			err:      createAzureResponseError("NotFound", "Resource not found", http.StatusNotFound),
+			expected: true,
+		},
+		{
+			name:     "400 InvalidParameter with 'Cannot find any valid machines' message",
+			err:      createAzureResponseError("InvalidParameter", "Cannot find any valid machines in the pool", http.StatusBadRequest),
+			expected: true,
+		},
+		{
+			name:     "400 InvalidParameter with different message",
+			err:      createAzureResponseError("InvalidParameter", "Some other validation error", http.StatusBadRequest),
+			expected: false,
+		},
+		{
+			name:     "400 with different error code",
+			err:      createAzureResponseError("ValidationError", "Cannot find any valid machines", http.StatusBadRequest),
+			expected: false,
+		},
+		{
+			name:     "500 Internal Server Error",
+			err:      createAzureResponseError("InternalServerError", "Server error", http.StatusInternalServerError),
+			expected: false,
+		},
+		{
+			name:     "403 Forbidden",
+			err:      createAzureResponseError("Forbidden", "Access denied", http.StatusForbidden),
+			expected: false,
+		},
+		{
+			name:     "401 Unauthorized",
+			err:      createAzureResponseError("Unauthorized", "Authentication required", http.StatusUnauthorized),
+			expected: false,
+		},
+		{
+			name:     "429 Too Many Requests",
+			err:      createAzureResponseError("TooManyRequests", "Rate limited", http.StatusTooManyRequests),
+			expected: false,
+		},
+		{
+			name:     "standard Go error",
+			err:      fmt.Errorf("standard error"),
+			expected: false,
+		},
+	}
 
-				errorDetails, pollingErr, done := HandleProvisioningState(ctx, aksMachine)
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+			g := NewWithT(t)
 
-				Expect(errorDetails).To(BeNil())
-				Expect(pollingErr).To(BeNil())
-				Expect(done).To(BeFalse())
-			})
+			result := IsAKSMachineOrMachinesPoolNotFound(tt.err)
 
-			It("should return done=false for Updating state", func() {
-				aksMachine.Properties.ProvisioningState = lo.ToPtr(consts.ProvisioningStateUpdating)
-
-				errorDetails, pollingErr, done := HandleProvisioningState(ctx, aksMachine)
-
-				Expect(errorDetails).To(BeNil())
-				Expect(pollingErr).To(BeNil())
-				Expect(done).To(BeFalse())
-			})
+			g.Expect(result).To(Equal(tt.expected))
 		})
-
-		Context("terminal success state", func() {
-			It("should return done=true for Succeeded state", func() {
-				aksMachine.Properties.ProvisioningState = lo.ToPtr(consts.ProvisioningStateSucceeded)
-
-				errorDetails, pollingErr, done := HandleProvisioningState(ctx, aksMachine)
-
-				Expect(errorDetails).To(BeNil())
-				Expect(pollingErr).To(BeNil())
-				Expect(done).To(BeTrue())
-			})
-		})
-
-		Context("terminal canceled state", func() {
-			It("should return error for Deleting state", func() {
-				aksMachine.Properties.ProvisioningState = lo.ToPtr(consts.ProvisioningStateDeleting)
-
-				errorDetails, pollingErr, done := HandleProvisioningState(ctx, aksMachine)
-
-				Expect(errorDetails).To(BeNil())
-				Expect(pollingErr).To(HaveOccurred())
-				Expect(pollingErr.Error()).To(ContainSubstring("canceled provisioning state"))
-				Expect(pollingErr.Error()).To(ContainSubstring(consts.ProvisioningStateDeleting))
-				Expect(done).To(BeTrue())
-			})
-		})
-
-		Context("terminal failed state", func() {
-			It("should return error details when ProvisioningError is present", func() {
-				aksMachine.Properties.ProvisioningState = lo.ToPtr(consts.ProvisioningStateFailed)
-				aksMachine.Properties.Status = &armcontainerservice.MachineStatus{
-					ProvisioningError: &armcontainerservice.ErrorDetail{
-						Code:    lo.ToPtr("QuotaExceeded"),
-						Message: lo.ToPtr("Quota exceeded for VM family"),
-					},
-				}
-
-				errorDetails, pollingErr, done := HandleProvisioningState(ctx, aksMachine)
-
-				Expect(errorDetails).ToNot(BeNil())
-				Expect(errorDetails.Code).To(Equal(lo.ToPtr("QuotaExceeded")))
-				Expect(errorDetails.Message).To(Equal(lo.ToPtr("Quota exceeded for VM family")))
-				Expect(pollingErr).To(BeNil())
-				Expect(done).To(BeTrue())
-			})
-
-			It("should return error when ProvisioningError is nil", func() {
-				aksMachine.Properties.ProvisioningState = lo.ToPtr(consts.ProvisioningStateFailed)
-				aksMachine.Properties.Status = &armcontainerservice.MachineStatus{
-					ProvisioningError: nil,
-				}
-
-				errorDetails, pollingErr, done := HandleProvisioningState(ctx, aksMachine)
-
-				Expect(errorDetails).To(BeNil())
-				Expect(pollingErr).To(HaveOccurred())
-				Expect(pollingErr.Error()).To(ContainSubstring("ProvisioningError is nil"))
-				Expect(pollingErr.Error()).To(ContainSubstring(consts.ProvisioningStateFailed))
-				Expect(done).To(BeTrue())
-			})
-
-			It("should return error when Status is nil", func() {
-				aksMachine.Properties.ProvisioningState = lo.ToPtr(consts.ProvisioningStateFailed)
-				aksMachine.Properties.Status = nil
-
-				errorDetails, pollingErr, done := HandleProvisioningState(ctx, aksMachine)
-
-				Expect(errorDetails).To(BeNil())
-				Expect(pollingErr).To(HaveOccurred())
-				Expect(pollingErr.Error()).To(ContainSubstring("ProvisioningError is nil"))
-				Expect(done).To(BeTrue())
-			})
-		})
-
-		Context("unrecognized states", func() {
-			It("should return done=false for unknown provisioning state", func() {
-				aksMachine.Properties.ProvisioningState = lo.ToPtr("UnknownState")
-
-				errorDetails, pollingErr, done := HandleProvisioningState(ctx, aksMachine)
-
-				Expect(errorDetails).To(BeNil())
-				Expect(pollingErr).To(BeNil())
-				Expect(done).To(BeFalse())
-			})
-
-			It("should return done=false for empty provisioning state", func() {
-				aksMachine.Properties.ProvisioningState = lo.ToPtr("")
-
-				errorDetails, pollingErr, done := HandleProvisioningState(ctx, aksMachine)
-
-				Expect(errorDetails).To(BeNil())
-				Expect(pollingErr).To(BeNil())
-				Expect(done).To(BeFalse())
-			})
-		})
-	})
-
-	Describe("IsAKSMachineOrMachinesPoolNotFound", func() {
-		Context("nil or no error", func() {
-			It("should return false for nil error", func() {
-				result := IsAKSMachineOrMachinesPoolNotFound(nil)
-
-				Expect(result).To(BeFalse())
-			})
-		})
-
-		Context("404 Not Found errors", func() {
-			It("should return true for 404 StatusNotFound", func() {
-				err := createAzureResponseError("NotFound", "Resource not found", http.StatusNotFound)
-
-				result := IsAKSMachineOrMachinesPoolNotFound(err)
-
-				Expect(result).To(BeTrue())
-			})
-		})
-
-		Context("400 Bad Request with specific message", func() {
-			It("should return true for InvalidParameter with 'Cannot find any valid machines' message", func() {
-				err := createAzureResponseError("InvalidParameter", "Cannot find any valid machines in the pool", http.StatusBadRequest)
-
-				result := IsAKSMachineOrMachinesPoolNotFound(err)
-
-				Expect(result).To(BeTrue())
-			})
-
-			It("should return false for InvalidParameter with different message", func() {
-				err := createAzureResponseError("InvalidParameter", "Some other validation error", http.StatusBadRequest)
-
-				result := IsAKSMachineOrMachinesPoolNotFound(err)
-
-				Expect(result).To(BeFalse())
-			})
-
-			It("should return false for BadRequest with different error code", func() {
-				err := createAzureResponseError("ValidationError", "Cannot find any valid machines", http.StatusBadRequest)
-
-				result := IsAKSMachineOrMachinesPoolNotFound(err)
-
-				Expect(result).To(BeFalse())
-			})
-		})
-
-		Context("other error codes", func() {
-			It("should return false for 500 Internal Server Error", func() {
-				err := createAzureResponseError("InternalServerError", "Server error", http.StatusInternalServerError)
-
-				result := IsAKSMachineOrMachinesPoolNotFound(err)
-
-				Expect(result).To(BeFalse())
-			})
-
-			It("should return false for 403 Forbidden", func() {
-				err := createAzureResponseError("Forbidden", "Access denied", http.StatusForbidden)
-
-				result := IsAKSMachineOrMachinesPoolNotFound(err)
-
-				Expect(result).To(BeFalse())
-			})
-
-			It("should return false for 401 Unauthorized", func() {
-				err := createAzureResponseError("Unauthorized", "Authentication required", http.StatusUnauthorized)
-
-				result := IsAKSMachineOrMachinesPoolNotFound(err)
-
-				Expect(result).To(BeFalse())
-			})
-
-			It("should return false for 429 Too Many Requests", func() {
-				err := createAzureResponseError("TooManyRequests", "Rate limited", http.StatusTooManyRequests)
-
-				result := IsAKSMachineOrMachinesPoolNotFound(err)
-
-				Expect(result).To(BeFalse())
-			})
-		})
-
-		Context("non-Azure errors", func() {
-			It("should return false for standard Go error", func() {
-				err := fmt.Errorf("standard error")
-
-				result := IsAKSMachineOrMachinesPoolNotFound(err)
-
-				Expect(result).To(BeFalse())
-			})
-		})
-	})
-})
+	}
+}
 
 // createAzureResponseError creates an Azure ResponseError for testing purposes
 func createAzureResponseError(errorCode, errorMessage string, statusCode int) error {
