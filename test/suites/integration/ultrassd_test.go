4f13615aa58b4a594b9ced12cfd301f4629cbff3
Pablo Trivino <pablotrivino@microsoft.com>
2026-06-16 11:12:39 -0400

    e2e skeleton

---
+++ test/suites/integration/ultrassd_test.go
@@ -0,0 +1,52 @@
+/*
+Portions Copyright (c) Microsoft Corporation.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package integration_test
+
+import (
+	corev1 "k8s.io/api/core/v1"
+	coretest "sigs.k8s.io/karpenter/pkg/test"
+
+	"github.com/Azure/karpenter-provider-azure/pkg/apis/v1beta1"
+	. "github.com/onsi/ginkgo/v2"
+)
+
+var _ = Describe("UltraSSD", func() {
+	BeforeEach(func() {
+		if !env.IsMachineModeOrNPS() {
+			Skip("UltraSSD tests require NPS (Node Provisioning Service) - only supported in NAP/managed Karpenter mode")
+		}
+	})
+
+	It("should enable UltraSSD when explicitly enabled", func() {
+		enabled := true
+		nodeClass.Spec.UltraSSD = &v1beta1.UltraSSD{
+			Enabled: &enabled,
+		}
+
+		deployment := coretest.Deployment(coretest.DeploymentOptions{Replicas: 1})
+		env.ExpectCreated(nodeClass, nodePool, deployment)
+		pods := env.EventuallyExpectHealthyDeployment(deployment)
+
+		env.EventuallyExpectInitializedNodeCount("==", 1)
+		node := env.GetNode(pods[0].Spec.NodeName)
+		verifyUltraSSDOnNode(node, true)
+	})
+})
+
+func verifyUltraSSDOnNode(node *corev1.Node, expected bool) {
+	return
+}
