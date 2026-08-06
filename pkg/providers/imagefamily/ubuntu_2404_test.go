a3f2e318cc8125354abb05ff11983513646d0dda
Pablo Trivino <pablotrivino@microsoft.com>
2026-08-06 11:24:29 -0400

    test

--- pkg/providers/imagefamily/ubuntu_2404_test.go
+++ pkg/providers/imagefamily/ubuntu_2404_test.go
@@ -54,10 +54,12 @@ func TestUbuntu2404_DefaultImages(t *testing.T) {
 		g.Expect(images[2].Distro).To(Equal("aks-ubuntu-arm64-containerd-24.04-gen2"))
 	})
 
-	t.Run("should return empty images for TrustedLaunch", func(t *testing.T) {
+	t.Run("should return correct images for TrustedLaunch", func(t *testing.T) {
 		g := NewWithT(t)
 		images := ubuntu.DefaultImages(false, nil, true)
-		g.Expect(images).To(BeEmpty())
+		g.Expect(images).To(HaveLen(1))
+		g.Expect(images[0].ImageDefinition).To(Equal(imagefamily.Ubuntu2404Gen2TrustedLaunchImageDefinition))
+		g.Expect(images[0].Distro).To(Equal("aks-ubuntu-containerd-24.04-tl-gen2"))
 	})
 
 	t.Run("should return empty images for FIPS mode without SIG", func(t *testing.T) {
