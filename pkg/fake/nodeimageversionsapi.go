5a173893221760d2887df438e569c9a4ff8d9f8d
Pablo Trivino <46615580+PabloTriv@users.noreply.github.com>
2026-08-10 10:34:09 -0400

    Update pkg/fake/nodeimageversionsapi.go
    Co-authored-by: Bevan Arps <bevan.arps@microsoft.com>

--- pkg/fake/nodeimageversionsapi.go
+++ pkg/fake/nodeimageversionsapi.go
@@ -83,10 +83,10 @@ var (
 			Version:  lo.ToPtr("202512.18.0"),
 		},
 		{
-			FullName: lo.ToPtr("AKSUbuntu-2204gen2fipsTLcontainerd-202512.18.0"),
-			OS:       lo.ToPtr("AKSUbuntu"),
-			SKU:      lo.ToPtr("2204gen2fipsTLcontainerd"),
-			Version:  lo.ToPtr("202512.18.0"),
+			FullName: new("AKSUbuntu-2204gen2fipsTLcontainerd-202512.18.0"),
+			OS:       new("AKSUbuntu"),
+			SKU:      new("2204gen2fipsTLcontainerd"),
+			Version:  new("202512.18.0"),
 		},
 		{
 			FullName: lo.ToPtr("AKSUbuntuEdgeZone-2204containerd-202512.18.0"),
