716e9c9641b8148d026c21a3aca00470212e02e2
Maximiliano Uribe <muribefalcon@microsoft.com>
2026-09-24 18:21:58 +0000

    test: verify FIPS encryption propagation through resolver

--- pkg/providers/imagefamily/resolver_unit_test.go
+++ pkg/providers/imagefamily/resolver_unit_test.go
@@ -22,9 +22,11 @@ import (
 	"time"
 
 	. "github.com/onsi/gomega"
+	"github.com/samber/lo"
 	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/resource"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	karpv1 "sigs.k8s.io/karpenter/pkg/apis/v1"
 	"sigs.k8s.io/karpenter/pkg/cloudprovider"
 	"sigs.k8s.io/karpenter/pkg/scheduling"
 
@@ -32,6 +34,7 @@ import (
 	"github.com/Azure/karpenter-provider-azure/pkg/consts"
 	"github.com/Azure/karpenter-provider-azure/pkg/operator/options"
 	"github.com/Azure/karpenter-provider-azure/pkg/providers/imagefamily/bootstrap"
+	template "github.com/Azure/karpenter-provider-azure/pkg/providers/launchtemplate/parameters"
 )
 
 func TestRequiresFIPS1403Encryption(t *testing.T) {
@@ -46,6 +49,62 @@ func TestRequiresFIPS1403Encryption(t *testing.T) {
 	} {
 		t.Run(distro, func(t *testing.T) {
 			NewWithT(t).Expect(requiresFIPS1403Encryption(distro)).To(Equal(want))
+		})
+	}
+}
+
+// Exercise Resolve itself so dropping the resolved-distro capability assignment
+// fails even if the standalone helper and VM payload tests still pass.
+func TestResolveFIPS1403Encryption(t *testing.T) {
+	for _, tc := range []struct {
+		name            string
+		family          string
+		fipsMode        v1beta1.FIPSMode
+		version         string
+		imageDefinition string
+		want            bool
+	}{
+		{"explicit Ubuntu2204 FIPS", v1beta1.Ubuntu2204ImageFamily, v1beta1.FIPSModeFIPS, "1.35.0", Ubuntu2204Gen2FIPSImageDefinition, true},
+		{"explicit Ubuntu2204 non-FIPS", v1beta1.Ubuntu2204ImageFamily, v1beta1.FIPSModeDisabled, "1.35.0", Ubuntu2204Gen2ImageDefinition, false},
+		{"generic Ubuntu FIPS resolves to 2204", v1beta1.UbuntuImageFamily, v1beta1.FIPSModeFIPS, "1.35.0", Ubuntu2204Gen2FIPSImageDefinition, true},
+		{"generic Ubuntu FIPS resolves to 2004", v1beta1.UbuntuImageFamily, v1beta1.FIPSModeFIPS, "1.34.0", Ubuntu2004Gen2FIPSImageDefinition, false},
+	} {
+		t.Run(tc.name, func(t *testing.T) {
+			g := NewWithT(t)
+			imageID := "/subscriptions/00000000-0000-0000-0000-000000000001/resourceGroups/images/providers/Microsoft.Compute/galleries/AKSUbuntu/images/" + tc.imageDefinition + "/versions/202501.02.0"
+			nodeClass := &v1beta1.AKSNodeClass{
+				Spec: v1beta1.AKSNodeClassSpec{
+					ImageFamily:  lo.ToPtr(tc.family),
+					FIPSMode:     lo.ToPtr(tc.fipsMode),
+					OSDiskSizeGB: lo.ToPtr[int32](128),
+				},
+				Status: v1beta1.AKSNodeClassStatus{
+					KubernetesVersion: lo.ToPtr(tc.version),
+					Images: []v1beta1.NodeImage{{
+						ID: imageID,
+						Requirements: []corev1.NodeSelectorRequirement{
+							{Key: corev1.LabelArchStable, Operator: corev1.NodeSelectorOpIn, Values: []string{karpv1.ArchitectureAmd64}},
+							{Key: v1beta1.LabelSKUHyperVGeneration, Operator: corev1.NodeSelectorOpIn, Values: []string{v1beta1.HyperVGenerationV2}},
+						},
+					}},
+				},
+			}
+			nodeClass.StatusConditions().SetTrue(v1beta1.ConditionTypeKubernetesVersionReady)
+			nodeClass.StatusConditions().SetTrue(v1beta1.ConditionTypeImagesReady)
+			ctx := options.ToContext(context.Background(), &options.Options{
+				ProvisionMode: consts.ProvisionModeBootstrappingClient,
+				UseSIG:        true,
+			})
+			resolver := NewDefaultResolver(nil, nil, stubInstanceTypeProvider{}, nil)
+			parameters, err := resolver.Resolve(ctx, nodeClass, &karpv1.NodeClaim{},
+				localDNSTestInstanceType("Standard_D4s_v3", 4, 16384), &template.StaticParameters{
+					ClusterName:       "test-cluster",
+					KubernetesVersion: tc.version,
+					Arch:              karpv1.ArchitectureAmd64,
+				})
+			g.Expect(err).ToNot(HaveOccurred())
+			g.Expect(parameters.ImageID).To(Equal(imageID))
+			g.Expect(parameters.EnableFIPS1403Encryption).To(Equal(tc.want))
 		})
 	}
 }
