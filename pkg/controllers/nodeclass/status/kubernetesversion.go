30b0d661354b8a37c3ebf4cf86a0858a58e573df
Pablo Trivino <pablotrivino@microsoft.com>
2026-09-24 20:57:03 -0400

    add check

--- pkg/controllers/nodeclass/status/kubernetesversion.go
+++ pkg/controllers/nodeclass/status/kubernetesversion.go
@@ -86,9 +86,9 @@ func (r *KubernetesVersionReconciler) Reconcile(ctx context.Context, nodeClass *
 	}
 	nodeClass.Status.ObservedVersions.ControlPlaneKubernetesVersion = &goalK8sVersion
 
-	if _, reqK8sVer := requestedVersions(nodeClass); reqK8sVer != "" {
+	if reqImgVer, reqK8sVer := requestedVersions(nodeClass); reqK8sVer != "" {
 		// Handles case 0: requested Kubernetes version is pinned
-		valid, err := r.validatePinnedK8sVersion(ctx, nodeClass, reqK8sVer, goalK8sVersion)
+		valid, err := r.validatePinnedK8sVersion(ctx, nodeClass, reqK8sVer, reqImgVer, goalK8sVersion)
 		if err != nil {
 			return reconcile.Result{}, err
 		}
@@ -129,7 +129,7 @@ func (r *KubernetesVersionReconciler) Reconcile(ctx context.Context, nodeClass *
 
 // validatePinnedK8sVersion validates a requested Kubernetes version and updates the corresponding
 // NodeClass readiness conditions. Operational errors are returned.
-func (r *KubernetesVersionReconciler) validatePinnedK8sVersion(ctx context.Context, nodeClass *v1beta1.AKSNodeClass, reqK8sVer, controlPlaneVersion string) (bool, error) {
+func (r *KubernetesVersionReconciler) validatePinnedK8sVersion(ctx context.Context, nodeClass *v1beta1.AKSNodeClass, reqK8sVer, reqImgVer, controlPlaneVersion string) (bool, error) {
 	versionSemver, err := semver.Parse(reqK8sVer)
 	if err != nil {
 		nodeClass.StatusConditions().SetFalse(v1beta1.ConditionTypeKubernetesVersionReady, "KubernetesVersionInvalidFormat", fmt.Sprintf("invalid kubernetes version format: %v", err))
@@ -155,12 +155,39 @@ func (r *KubernetesVersionReconciler) validatePinnedK8sVersion(ctx context.Conte
 	}
 
 	currentK8sVersion := lo.FromPtr(nodeClass.Status.KubernetesVersion)
+	if !validateCurrentOrLatestImagePin(nodeClass, currentK8sVersion, reqK8sVer, reqImgVer) {
+		return false, nil
+	}
+
 	if currentK8sVersion != reqK8sVer {
 		log.FromContext(ctx).V(1).Info("requested Kubernetes version differs from current version", "currentKubernetesVersion", currentK8sVersion, "requestedKubernetesVersion", reqK8sVer)
 		nodeClass.StatusConditions().SetFalse(v1beta1.ConditionTypeImagesReady, "KubernetesPinning", "Performing kubernetes version change, need to get latest images")
 	}
 
 	return true, nil
+}
+
+func validateCurrentOrLatestImagePin(nodeClass *v1beta1.AKSNodeClass, currentK8sVersion, reqK8sVer, reqImgVer string) bool {
+	currentImageVersion := ""
+	if len(nodeClass.Status.Images) > 0 {
+		currentImageVersion = parseVersion(nodeClass.Status.Images[0].ID)
+	}
+	latestImageVersion := lo.FromPtr(nodeClass.Status.ObservedVersions.LatestImageVersion)
+
+	kubernetesVersionChanging := currentK8sVersion != "" && currentK8sVersion != reqK8sVer
+	pinningCurrentOrLatestImage := reqImgVer != "" &&
+		(reqImgVer == currentImageVersion || reqImgVer == latestImageVersion)
+
+	if kubernetesVersionChanging && pinningCurrentOrLatestImage {
+		nodeClass.StatusConditions().SetFalse(
+			v1beta1.ConditionTypeKubernetesVersionReady,
+			"KubernetesVersionImagePinIncompatible",
+			"cannot change Kubernetes version while pinning to the current or latest node image version",
+		)
+		return false
+	}
+
+	return true
 }
 
 func validateKubernetesVersionSkew(nodeClass *v1beta1.AKSNodeClass, version, controlPlaneVersion semver.Version) bool {
